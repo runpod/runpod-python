783b773a77712d46ba6c83f5d0c38a5785a63917
Justin Merrell <justin.merrell@adiaon.com>
2022-12-19 16:10:48 -0500

    Update test_pod_worker.py

--- tests/test_serverless/test_pod_worker.py
+++ tests/test_serverless/test_pod_worker.py
@@ -9,12 +9,10 @@ from runpod.serverless import pod_worker
 class TestPodWorker(unittest.TestCase):
     ''' Tests for pod_worker '''
 
-    @patch('runpod.serverless.modules.lifecycle.LifecycleManager.heartbeat_ping')
     @patch('runpod.serverless.modules.lifecycle.LifecycleManager')
-    def test_start_worker(self, mock_lifecycle, mock_heartbeat_ping):
+    def test_start_worker(self, mock_lifecycle):
         '''
         Tests start_worker
         '''
         pod_worker.start_worker()
         mock_lifecycle.assert_called_once_with()
-        mock_heartbeat_ping.assert_called_once_with()
