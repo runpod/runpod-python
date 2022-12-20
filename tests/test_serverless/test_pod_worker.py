a5ccb0552e710f9c7e1e2bcd7721a4e5e5961361
Justin Merrell <justin.merrell@adiaon.com>
2022-12-20 12:28:32 -0500

    fix: test shutil

--- tests/test_serverless/test_pod_worker.py
+++ tests/test_serverless/test_pod_worker.py
@@ -11,9 +11,10 @@ class TestPodWorker(unittest.TestCase):
     ''' Tests for pod_worker '''
 
     @patch('os.path.exists', return_value=False)
+    @patch('shutil.rmtree')
     @patch('runpod.serverless.modules.job.get')
     @patch('runpod.serverless.modules.lifecycle.LifecycleManager')
-    def test_start_worker(self, mock_worker_life, mock_job_get, mock_os_path_exists):
+    def test_start_worker(self, mock_worker_life, mock_job_get):
         '''
         Tests start_worker
         '''
@@ -25,4 +26,3 @@ class TestPodWorker(unittest.TestCase):
         mock_worker_life.assert_called_once_with()
         mock_worker_life.heartbeat_ping.assert_called_once_with()
         mock_job_get.assert_called_once_with(mock_worker_life.worker_id)
-        mock_os_path_exists.assert_called_once_with('output.zip')
