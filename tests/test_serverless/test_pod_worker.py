00cdf3103102d9dfcf78c16e09466059ef22c45a
Justin Merrell <justin.merrell@adiaon.com>
2022-12-21 15:29:32 -0500

    fix: test

--- tests/test_serverless/test_pod_worker.py
+++ tests/test_serverless/test_pod_worker.py
@@ -11,19 +11,15 @@ class TestPodWorker(unittest.TestCase):
     ''' Tests for pod_worker '''
 
     @patch('runpod.serverless.modules.job.get', return_value=None)
-    @patch('runpod.serverless.modules.lifecycle.LifecycleManager.heartbeat_ping')
     @patch('runpod.serverless.modules.lifecycle.LifecycleManager')
-    def test_start_worker(self, mock_worker_life, mock_worker_life_heartbeat, mock_job_get):
+    def test_start_worker(self, mock_worker_life, mock_job_get):
         '''
         Tests start_worker
         '''
-        os.environ['DEBUG'] = 'true'
-
         pod_worker.start_worker()
-        # print(os.environ.get('DEBUG', None))
 
         mock_worker_life.assert_called_once_with()
-        mock_worker_life_heartbeat.assert_called_once_with()
+        mock_worker_life.heartbeat_ping.assert_called_once_with()
         mock_job_get.assert_called_once_with(mock_worker_life.worker_id)
 
     # @patch('os.path.exists', return_value=False)
