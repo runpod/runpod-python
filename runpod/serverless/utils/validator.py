158884faa514fe55a539358a610e8088ff43332d
Justin Merrell <justin.merrell@adiaon.com>
2022-12-24 15:03:50 -0500

    fix: input usage

--- runpod/serverless/utils/validator.py
+++ runpod/serverless/utils/validator.py
@@ -4,7 +4,7 @@ Provides a function to validate the input to the model.
 '''
 
 
-def validate(input, validation_source):
+def validate(job_input, validation_source):
     '''
     Validates the input.
     Checks to see if the provided inputs match the expected types.
@@ -13,11 +13,11 @@ def validate(input, validation_source):
     input_errors = []
 
     for key, value in validation_source.items():
-        if value['required'] and key not in input:
+        if value['required'] and key not in job_input:
             input_errors.append(f"{key} is a required input.")
 
-        if key in input and not isinstance(input[key], value['type']):
+        if key in job_input and not isinstance(job_input[key], value['type']):
             input_errors.append(
-                f"{key} should be {value['type']} type, not {type(input[key])}.")
+                f"{key} should be {value['type']} type, not {type(job_input[key])}.")
 
     return input_errors
