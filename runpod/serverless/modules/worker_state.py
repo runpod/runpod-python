f644715545b346dbd3305858b4d60f08c10730b0
zeen <zhl146@gmail.com>
2022-12-30 14:02:55 -0500

    chore: linting

--- runpod/serverless/modules/worker_state.py
+++ runpod/serverless/modules/worker_state.py
@@ -1,16 +1,20 @@
+'''
+Handles getting stuff from env and updating global state like job id
+'''
+
 import os
 import uuid
 
-current_job_id = None
+CURRENT_JOB_ID = None
 
 worker_id = os.environ.get('RUNPOD_POD_ID', str(uuid.uuid4()))
 
 auth_header = {"Authorization": f"{os.environ.get('RUNPOD_AI_API_KEY')}"}
-job_get_url = str(os.environ.get('RUNPOD_WEBHOOK_GET_JOB')).replace(
+JOB_GET_URL = str(os.environ.get('RUNPOD_WEBHOOK_GET_JOB')).replace(
     '$ID', worker_id)
-job_done_url = str(os.environ.get(
+JOB_DONE_URL = str(os.environ.get(
     'RUNPOD_WEBHOOK_POST_OUTPUT'))
-job_done_url = job_done_url.replace(
+JOB_DONE_URL = JOB_DONE_URL.replace(
     '$RUNPOD_POD_ID', worker_id)
 
 webhook_ping = os.environ.get('RUNPOD_WEBHOOK_PING', None)
@@ -20,13 +24,22 @@ ping_url = webhook_ping.replace(
 
 
 def get_current_job_id():
-    return current_job_id
+    '''
+    get current job id
+    '''
+    return CURRENT_JOB_ID
 
 
 def get_done_url():
-    return job_done_url.replace('$ID', current_job_id)
+    '''
+    constructs done url using current job id
+    '''
+    return JOB_DONE_URL.replace('$ID', CURRENT_JOB_ID)
 
 
 def set_job_id(new_job_id):
-    global current_job_id
-    current_job_id = new_job_id
+    '''
+    sets current job id
+    '''
+    global CURRENT_JOB_ID  # pylint: disable=global-statement
+    CURRENT_JOB_ID = new_job_id
