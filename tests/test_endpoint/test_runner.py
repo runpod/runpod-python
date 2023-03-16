bec6756b9accc60d7ed46c77a0b0b266cce1f863
Justin Merrell <merrelljustin@gmail.com>
2023-03-16 00:47:54 -0400

    Update test_runner.py

--- tests/test_endpoint/test_runner.py
+++ tests/test_endpoint/test_runner.py
@@ -1,55 +1,50 @@
-import unittest
-from unittest.mock import patch, Mock
-import runpod
+''' Tests for runpod | serverless | modules | endpoint.py '''
+# pylint: disable=R0903,W0613
 
-runpod.api_key = "YOUR_API_KEY"
-endpoint = runpod.Endpoint("ENDPOINT_ID")
+import unittest
+from unittest.mock import patch, MagicMock
+import runpod
 
 
 class TestEndpoint(unittest.TestCase):
+    ''' Tests for Endpoint '''
 
-    @patch('requests.post')
+    @patch('runpod.serverless.modules.endpoint.requests.post')
     def test_run(self, mock_post):
-        # Configure the mock response for the requests.post() call
-        mock_post.return_value = Mock()
-        mock_post.return_value.json.return_value = {"id": "JOB_ID"}
+        '''
+        Tests Endpoint.run
+        '''
+        mock_response = MagicMock()
+        mock_response.json.return_value = {
+            "job_id": "123",
+            "status": "in_progress"
+        }
+        mock_post.return_value = mock_response
 
-        # Test that the run method returns a Job object
-        job = endpoint.run({"YOUR_MODEL_INPUT_JSON": "YOUR_MODEL_INPUT_VALUE"})
-        self.assertIsInstance(job, runpod.endpoint.Job)
+        endpoint = runpod.Endpoint("ENDPOINT_ID")
 
-    @patch('requests.post')
+        request_data = {"YOUR_MODEL_INPUT_JSON": "YOUR_MODEL_INPUT_VALUE"}
+        run_request = endpoint.run(request_data)
+
+        self.assertEqual(run_request.job_id, "123")
+        self.assertEqual(run_request.status(), "in_progress")
+
+    @patch('runpod.serverless.modules.endpoint.requests.post')
     def test_run_sync(self, mock_post):
-        # Configure the mock response for the requests.post() call
-        mock_post.return_value = Mock()
-        mock_post.return_value.json.return_value = {"output": {
-            "YOUR_MODEL_OUTPUT_JSON": "YOUR_MODEL_OUTPUT_VALUE"}}
+        '''
+        Tests Endpoint.run_sync
+        '''
+        mock_response = MagicMock()
+        mock_response.json.return_value = {
+            "job_id": "123",
+            "status": "completed",
+            "output": {"result": "YOUR_MODEL_OUTPUT_VALUE"}
+        }
+        mock_post.return_value = mock_response
 
-        # Test that the run_sync method returns output within 90 seconds
-        output = endpoint.run_sync({"YOUR_MODEL_INPUT_JSON": "YOUR_MODEL_INPUT_VALUE"})
-        self.assertTrue(output is not None)
+        endpoint = runpod.Endpoint("ENDPOINT_ID")
 
+        request_data = {"YOUR_MODEL_INPUT_JSON": "YOUR_MODEL_INPUT_VALUE"}
+        run_request = endpoint.run_sync(request_data)
 
-class TestJob(unittest.TestCase):
-
-    @patch('requests.get')
-    def setUp(self, mock_get):
-        # Configure the mock response for the requests.get() call
-        mock_get.return_value = Mock()
-        mock_get.return_value.json.side_effect = [{"status": "PENDING"}, {
-            "status": "COMPLETED", "output": {"YOUR_MODEL_OUTPUT_JSON": "YOUR_MODEL_OUTPUT_VALUE"}}]
-
-        # Create a new job for testing
-        self.job = runpod.endpoint.Job(endpoint_id="ENDPOINT_ID", job_id="JOB_ID")
-
-    @patch('requests.get')
-    def test_status(self, mock_get):
-        # Test that the status method returns a string
-        status = self.job.status()
-        self.assertIsInstance(status, str)
-
-    @patch('requests.get')
-    def test_output(self, mock_get):
-        # Test that the output method returns a dict
-        output = self.job.output()
-        self.assertIsInstance(output, dict)
+        self.assertEqual(run_request, {"result": "YOUR_MODEL_OUTPUT_VALUE"})
