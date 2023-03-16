0d892a95f140cbdf78d99391d995c382ad4f587a
Justin Merrell <merrelljustin@gmail.com>
2023-03-15 23:33:00 -0400

    Update test_runner.py

--- tests/test_endpoint/test_runner.py
+++ tests/test_endpoint/test_runner.py
@@ -1,120 +1,107 @@
-'''
-Tests for runpod | endpoint | runner
-'''
-
 import unittest
-from unittest.mock import patch
-from requests.exceptions import Timeout
+from unittest.mock import MagicMock, patch
 
-from runpod import api_key, endpoint_url_base
-from runpod.endpoint import Endpoint
+import requests
+
+from runpod import Endpoint
 from runpod.endpoint.runner import Job
 
 
 class TestEndpoint(unittest.TestCase):
-    ''' Tests for the Endpoint class. '''
 
     def setUp(self):
-        '''
-        Sets up the test.
-        '''
         self.endpoint_id = "test_endpoint"
-        self.endpoint_url = f"{endpoint_url_base}/{self.endpoint_id}/run"
+        self.endpoint_url_base = "https://example.com/api"
+        self.api_key = "test_api_key"
+        self.endpoint_url = f"{self.endpoint_url_base}/{self.endpoint_id}/run"
         self.headers = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {api_key}"
+            "Authorization": f"Bearer {self.api_key}"
         }
-        self.endpoint = Endpoint(self.endpoint_id)
 
-    @patch('runpod.endpoint.runner.requests.post')
-    def test_run(self, mock_post):
-        '''
-        Tests the run method.
-        '''
-        mock_post.return_value.json.return_value = {"id": "test_job_id"}
-        mock_post.return_value.status_code = 200
+    def test_run(self):
+        endpoint_input = {"input_1": 1, "input_2": 2}
+        expected_job_id = "test_job_id"
+        expected_response = {"id": expected_job_id}
 
-        endpoint_input = {"input": "test_input"}
+        with patch.object(requests, "post") as mock_post:
+            mock_post.return_value.json.return_value = expected_response
+            endpoint = Endpoint(self.endpoint_id)
 
-        returned_job = self.endpoint.run(endpoint_input)
-        expected_job = Job(self.endpoint_id, returned_job.job_id)
+            job = endpoint.run(endpoint_input)
 
-        self.assertEqual(returned_job, expected_job)
-        mock_post.assert_called_once_with(
-            self.endpoint_url, headers=self.headers, json=endpoint_input, timeout=10
-        )
-
-    @patch('runpod.endpoint.runner.requests.post')
-    def test_run_timeout(self, mock_post):
-        '''
-        Tests the run method with a timeout.
-        '''
-        mock_post.side_effect = Timeout
-
-        endpoint_input = {"input": "test_input"}
-
-        with self.assertRaises(Timeout):
-            self.endpoint.run(endpoint_input)
-        mock_post.assert_called_once_with(
-            self.endpoint_url, headers=self.headers, json=endpoint_input, timeout=10
-        )
-
-    @patch('runpod.endpoint.runner.requests.post')
-    def test_run_sync(self, mock_post):
-        '''
-        Tests the run_sync method.
-        '''
-        mock_post.return_value.json.return_value = {"output": "test_output"}
-        mock_post.return_value.status_code = 200
-
-        endpoint_input = {"input": "test_input"}
-
-        self.assertEqual(self.endpoint.run_sync(endpoint_input), {"output": "test_output"})
-        mock_post.assert_called_once_with(
-            self.endpoint_url, headers=self.headers, json=endpoint_input, timeout=100
-        )
-
-    @patch('runpod.endpoint.runner.requests.get')
-    def test_status(self, mock_get):
-        '''
-        Tests the status method.
-        '''
-        mock_get.return_value.json.return_value = {"status": "COMPLETED"}
-        mock_get.return_value.status_code = 200
-
-        expected_status = "COMPLETED"
-        test_job = Job(self.endpoint_id, "test_job_id")
-
-        self.assertEqual(test_job.status(), expected_status)
-        mock_get.assert_called_once_with(
-            f"{endpoint_url_base}/{self.endpoint_id}/status/{test_job.job_id}",
-            headers=self.headers, timeout=10
-        )
-
-    @patch('runpod.endpoint.runner.time.sleep')
-    @patch('runpod.endpoint.runner.requests.get')
-    def test_output(self, mock_get, mock_sleep):
-        '''
-        Tests the output method.
-        '''
-        mock_get.return_value.json.side_effect = [
-            {"status": "RUNNING"},
-            {"status": "COMPLETED", "output": "test_output"}
-        ]
-        mock_get.return_value.status_code = 200
-
-        expected_output = "test_output"
-        test_job = Job(self.endpoint_id, "test_job_id")
-
-        self.assertEqual(test_job.output(), expected_output)
-        mock_sleep.assert_called_with(.1)
-        mock_get.assert_has_calls([
-            unittest.mock.call(
-                f"{endpoint_url_base}/{self.endpoint_id}/status/{test_job.job_id}",
-                headers=self.headers, timeout=10
-            ),
-            unittest.mock.call(
-                f"{endpoint_url_base}/{self.endpoint_id}/status/{test_job.job_id}",
-                headers=self.headers, timeout=10
+            mock_post.assert_called_once_with(
+                self.endpoint_url,
+                headers=self.headers,
+                json={"input": endpoint_input},
+                timeout=10,
             )
-        ])
+            self.assertEqual(job.endpoint_id, self.endpoint_id)
+            self.assertEqual(job.job_id, expected_job_id)
+
+    def test_run_sync(self):
+        endpoint_input = {"input_1": 1, "input_2": 2}
+        expected_response = {"status": "COMPLETED", "output": {"result": 42}}
+
+        with patch.object(requests, "post") as mock_post:
+            mock_post.return_value.json.return_value = expected_response
+            endpoint = Endpoint(self.endpoint_id)
+
+            response = endpoint.run_sync(endpoint_input)
+
+            mock_post.assert_called_once_with(
+                self.endpoint_url,
+                headers=self.headers,
+                json={"input": endpoint_input},
+                timeout=100,
+            )
+            self.assertEqual(response, expected_response)
+
+
+class TestJob(unittest.TestCase):
+
+    def setUp(self):
+        self.endpoint_id = "test_endpoint"
+        self.job_id = "test_job_id"
+        self.endpoint_url_base = "https://example.com/api"
+        self.api_key = "test_api_key"
+        self.status_url = f"{self.endpoint_url_base}/{self.endpoint_id}/status/{self.job_id}"
+        self.output_url = f"{self.endpoint_url_base}/{self.endpoint_id}/output/{self.job_id}"
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+
+    def test_status(self):
+        expected_status = "COMPLETED"
+        expected_response = {"status": expected_status}
+
+        with patch.object(requests, "get") as mock_get:
+            mock_get.return_value.json.return_value = expected_response
+            job = Job(self.endpoint_id, self.job_id)
+
+            status = job.status()
+
+            mock_get.assert_called_once_with(
+                self.status_url,
+                headers=self.headers,
+                timeout=10,
+            )
+            self.assertEqual(status, expected_status)
+
+    def test_output(self):
+        expected_output = {"result": 42}
+        expected_response = {"output": expected_output}
+
+        with patch.object(requests, "get") as mock_get:
+            mock_get.return_value.json.return_value = expected_response
+            job = Job(self.endpoint_id, self.job_id)
+
+            output = job.output()
+
+            mock_get.assert_called_once_with(
+                self.output_url,
+                headers=self.headers,
+                timeout=10,
+            )
+            self.assertEqual(output, expected_output)
