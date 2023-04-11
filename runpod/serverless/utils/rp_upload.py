a25ea506adf0572111a3871ec2e012bc434614d2
Justin Merrell <merrelljustin@gmail.com>
2023-04-11 10:39:31 -0400

    Update rp_upload.py

--- runpod/serverless/utils/rp_upload.py
+++ runpod/serverless/utils/rp_upload.py
@@ -186,7 +186,7 @@ def bucket_upload(job_id, file_list, bucket_creds):
 
 
 # ------------------------- Single File Bucket Upload ------------------------ #
-def upload_file_to_bucket(file_name: str, file_location: str, bucket_creds: dict) -> str:
+def upload_file_to_bucket(file_name: str, file_location: str, bucket_creds: Optional[dict] = None) -> str:
     '''
     Uploads a single file to bucket storage and returns a presigned URL.
     '''
@@ -211,7 +211,7 @@ def upload_file_to_bucket(file_name: str, file_location: str, bucket_creds: dict
 
 
 # --------------------------- Upload Memory Object --------------------------- #
-def upload_in_memory_object(file_name: str, file_data: bytes, bucket_creds: dict) -> str:
+def upload_in_memory_object(file_name: str, file_data: bytes, bucket_creds: Optional[dict] = None) -> str:
     '''
     Uploads an in-memory object (bytes) to bucket storage and returns a presigned URL.
     '''
