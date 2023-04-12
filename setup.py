9510c09c28318dd61d075d312b8ceec7b190a961
Justin Merrell <merrelljustin@gmail.com>
2023-04-11 23:45:31 -0400

    feat: auto release versioning

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ Called to setup the runpod-python package.
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        use_scm_version=True,
+        setup_requires=['setuptools_scm']
+    )
