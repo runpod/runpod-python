a9609db364ecdc4923c40154f3946368881de93f
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2023-06-08 16:06:36 +0300

    feat: updated api version

--- runpod/__init__.py
+++ runpod/__init__.py
@@ -12,4 +12,4 @@ api_key = None  # pylint: disable=invalid-name
 
 api_url_base = "https://api.runpod.io"  # pylint: disable=invalid-name
 
-endpoint_url_base = "https://api.runpod.ai/v1"  # pylint: disable=invalid-name
+endpoint_url_base = "https://api.runpod.ai/v2"  # pylint: disable=invalid-name
