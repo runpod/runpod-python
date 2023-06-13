041bb729d1cd065e4258c9b2e4b6d0185bd7b20c
Justin Merrell <merrelljustin@gmail.com>
2023-06-12 21:05:05 -0400

    feat: cuda tools

---
+++ runpod/serverless/utils/rp_cuda.py
@@ -0,0 +1,18 @@
+"""
+Provides some of the torch.cuda functionality without requiring torch.
+"""
+
+import subprocess
+
+
+def is_cuda_available():
+    '''
+    Returns True if CUDA is available, False otherwise.
+    '''
+    try:
+        output = subprocess.check_output("nvidia-smi", shell=True)
+        if "NVIDIA-SMI" in output.decode():
+            return True
+    except Exception:  # pylint: disable=broad-except
+        pass
+    return False
