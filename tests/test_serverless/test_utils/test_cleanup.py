8d263935c2c00886dd89a428123ba6a3f1988fba
Justin Merrell <merrelljustin@gmail.com>
2023-07-09 02:57:35 -0400

    Update test_cleanup.py

--- tests/test_serverless/test_utils/test_cleanup.py
+++ tests/test_serverless/test_utils/test_cleanup.py
@@ -10,7 +10,8 @@ def test_clean_no_folders():
     '''
     Test clean() with no folders.
     '''
-    with patch('shutil.rmtree') as mock_rmtree, patch('os.remove') as mock_remove, patch('os.path.exists', return_value=False):
+    with patch('shutil.rmtree') as mock_rmtree, patch('os.remove') as mock_remove, \
+        patch('os.path.exists', return_value=False):
         rp_cleanup.clean()
         assert mock_rmtree.call_count == 3
         mock_rmtree.assert_any_call("input_objects", ignore_errors=True)
@@ -22,7 +23,8 @@ def test_clean_with_output_zip():
     '''
     Test clean() with output.zip.
     '''
-    with patch('shutil.rmtree') as mock_rmtree, patch('os.remove') as mock_remove, patch('os.path.exists', return_value=True):
+    with patch('shutil.rmtree') as mock_rmtree, patch('os.remove') as mock_remove, \
+        patch('os.path.exists', return_value=True):
         rp_cleanup.clean()
         assert mock_rmtree.call_count == 3
         mock_remove.assert_called_once_with('output.zip')
@@ -31,7 +33,8 @@ def test_clean_with_folders():
     '''
     Test clean() with folders.
     '''
-    with patch('shutil.rmtree') as mock_rmtree, patch('os.remove') as mock_remove, patch('os.path.exists', return_value=False):
+    with patch('shutil.rmtree') as mock_rmtree, patch('os.remove') as mock_remove, \
+        patch('os.path.exists', return_value=False):
         rp_cleanup.clean(['test_folder1', 'test_folder2'])
         assert mock_rmtree.call_count == 5
         mock_rmtree.assert_any_call('test_folder1', ignore_errors=True)
