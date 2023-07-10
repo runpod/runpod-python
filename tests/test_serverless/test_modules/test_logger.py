1d438036e8fe18827476f30b519c0a0c88815329
Justin Merrell <merrelljustin@gmail.com>
2023-07-10 14:00:30 -0400

    feat: more tests

---
+++ tests/test_serverless/test_modules/test_logger.py
@@ -0,0 +1,110 @@
+''' Tests for runpod.serverless.modules.rp_logger '''
+
+import unittest
+from unittest.mock import patch
+
+from runpod.serverless.modules import rp_logger
+
+
+class TestLogger(unittest.TestCase):
+    ''' Tests for rp_logger '''
+
+    def setUp(self) -> None:
+        '''
+        Set up the logger for each test
+        '''
+        self.logger = rp_logger.RunPodLogger()
+
+    def test_default_log_level(self):
+        '''
+        Tests that the default log level is DEBUG
+        '''
+        defult_logger = rp_logger.RunPodLogger()
+
+        self.assertEqual(defult_logger.level, "DEBUG")
+
+    def test_singleton(self):
+        '''
+        Tests that the logger is a singleton
+        '''
+        logger1 = rp_logger.RunPodLogger()
+        logger2 = rp_logger.RunPodLogger()
+
+        self.assertIs(logger1, logger2)
+
+    def test_set_log_level(self):
+        '''
+        Tests that the log level can be set
+        '''
+        logger = rp_logger.RunPodLogger()
+
+        logger.set_level("INFO")
+        self.assertEqual(logger.level, "INFO")
+
+        logger.set_level("WARN")
+        self.assertEqual(logger.level, "WARN")
+
+        logger.set_level(2)
+        self.assertEqual(logger.level, "INFO")
+
+    def test_call_log(self):
+        '''
+        Tests that the logger can be called and logs the message to stdout if the log level is set.
+        '''
+        log = rp_logger.RunPodLogger()
+
+        with patch("runpod.serverless.modules.rp_logger.RunPodLogger.log") as mock_log:
+
+            log.warn("Test log message")
+
+            mock_log.assert_called_once_with("Test log message", "WARN")
+
+        log.set_level(0)
+        with patch("runpod.serverless.modules.rp_logger.RunPodLogger.log") as mock_log, \
+            patch("builtins.print") as mock_print:
+
+            log.debug("Test log message")
+
+            mock_log.assert_called_once_with("Test log message", "DEBUG")
+            mock_print.assert_not_called()
+
+        # Reset log level
+        log.set_level("DEBUG")
+
+    def test_invalid_debug_level(self):
+        '''
+        Tests that an invalid debug level raises an exception
+        '''
+        logger = rp_logger.RunPodLogger()
+
+        with self.assertRaises(ValueError):
+            logger.set_level("INVALID")
+
+        with self.assertRaises(ValueError):
+            logger.set_level([])
+
+    def test_debug_level_int(self):
+        '''
+        Tests that the debug level can be set using an int
+        '''
+        int_logger = rp_logger.RunPodLogger()
+
+        with self.assertRaises(ValueError):
+            int_logger.set_level(69)
+
+    def test_log_secret(self):
+        '''
+        Tests that the secret method censors secrets.
+        Captures stdout and checks that the secret is censored.
+        '''
+        with patch("runpod.serverless.modules.rp_logger.RunPodLogger.log") as mock_log:
+            self.logger.secret("test_secret", "test_secret_value")
+            mock_log.assert_called_once_with("test_secret: t***************e", "INFO")
+
+    def test_log_tip(self):
+        '''
+        Tests that the tip method logs a tip.
+        '''
+        with patch("runpod.serverless.modules.rp_logger.RunPodLogger.log") as mock_log:
+            self.logger.tip("test_tip")
+            mock_log.assert_called_once_with("test_tip", "TIP")
---
+++ tests/test_serverless/test_utils/test_debugger.py
@@ -0,0 +1,118 @@
+'''
+Unit tests for the debugger utility functions.
+'''
+
+import time
+import unittest
+import importlib
+from unittest.mock import patch
+
+from runpod.serverless.utils import rp_debugger
+from runpod.serverless.utils.rp_debugger import(
+    Checkpoints, LineTimer, FunctionTimer, get_debugger_output, clear_debugger_output
+)
+
+
+class TestDebugger(unittest.TestCase):
+    ''' Unit tests for the debugger utility functions. '''
+
+    def setUp(self):
+        self.checkpoints = Checkpoints()
+        self.checkpoints.clear()
+
+    @patch('runpod.serverless.utils.rp_debugger.cpuinfo.get_cpu_info')
+    def test_key_error(self, mock_get_cpu_info):
+        '''
+        Test that a KeyError is raised when an invalid key is used.
+        '''
+        mock_get_cpu_info.side_effect = KeyError('Test Error')
+
+        importlib.reload(rp_debugger)
+
+        assert mock_get_cpu_info.called
+        self.assertEqual(rp_debugger.PROCESSOR, 'Unable to get processor info.')
+
+    def test_checkpoints(self):
+        '''
+        Test that checkpoints are added and stopped correctly.
+        '''
+        self.checkpoints.add('checkpoint1')
+        self.checkpoints.start('checkpoint1')
+        time.sleep(0.1)
+        self.checkpoints.stop('checkpoint1')
+
+        output = get_debugger_output()['timestamps']
+
+        self.assertEqual(len(output), 1)
+        self.assertEqual(output[0]['name'], 'checkpoint1')
+        self.assertGreater(output[0]['duration_ms'], 0)
+
+    def test_get_checkpoints(self):
+        '''
+        Test that checkpoints are added and stopped correctly.
+        '''
+        self.checkpoints.add('checkpoint1')
+        checkpoint_list = self.checkpoints.get_checkpoints()
+
+        self.assertEqual(len(checkpoint_list), 0)
+
+
+    def test_checkpoints_exception(self):
+        '''
+        Test that a KeyError is raised when an invalid checkpoint is used.
+        '''
+        self.assertRaises(KeyError, self.checkpoints.start, 'nonexistent')
+        self.assertRaises(KeyError, self.checkpoints.stop, 'nonexistent')
+
+        self.checkpoints.add('checkpoint2')
+        self.assertRaises(KeyError, self.checkpoints.add, 'checkpoint2')
+        self.assertRaises(KeyError, self.checkpoints.stop, 'checkpoint2')
+
+    def test_line_timer(self):
+        '''
+        Test that the line timer works correctly.
+        '''
+        with LineTimer('line_timer'):
+            time.sleep(0.1)
+
+        output = get_debugger_output()['timestamps']
+
+        self.assertEqual(len(output), 1)
+        self.assertEqual(output[0]['name'], 'line_timer')
+        self.assertGreater(output[0]['duration_ms'], 0)
+
+    def test_function_timer(self):
+        '''
+        Test that the function timer works correctly.
+        '''
+        @FunctionTimer
+        def func_to_time():
+            time.sleep(0.1)
+
+        func_to_time()
+
+        output = get_debugger_output()['timestamps']
+
+        self.assertEqual(len(output), 1)
+        self.assertEqual(output[0]['name'], 'func_to_time')
+        self.assertGreater(output[0]['duration_ms'], 0)
+
+
+    def test_clear_debugger_output(self):
+        '''
+        Test that the debugger output is cleared correctly.
+        '''
+        self.checkpoints.add('checkpoint1')
+        self.checkpoints.start('checkpoint1')
+        self.checkpoints.stop('checkpoint1')
+
+        checkpoint_list = self.checkpoints.get_checkpoints()
+        self.assertEqual(len(checkpoint_list), 1)
+
+        clear_debugger_output()
+
+        checkpoint_list = self.checkpoints.get_checkpoints()
+        self.assertEqual(len(checkpoint_list), 0)
+
+if __name__ == "__main__":
+    unittest.main()
