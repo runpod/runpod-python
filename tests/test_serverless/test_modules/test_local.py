38ed6ee862193ec7defd3f7de0bf968fea12b2f0
Justin Merrell <merrelljustin@gmail.com>
2023-07-16 22:20:13 -0400

    Update test_local.py

--- tests/test_serverless/test_modules/test_local.py
+++ tests/test_serverless/test_modules/test_local.py
@@ -1,8 +1,8 @@
 ''' Tests for rp_local.py '''
 
-import pytest
 import unittest
 from unittest.mock import patch, mock_open
+import pytest
 
 from runpod.serverless.modules import rp_local
 
@@ -23,9 +23,12 @@ class TestRunLocal(unittest.TestCase):
                 }
             }
         }
-        with self.assertRaises(SystemExit) as cm:
+        with self.assertRaises(SystemExit) as sys_exit:
             rp_local.run_local(config)
-        self.assertEqual(cm.exception.code, 0)
+        self.assertEqual(sys_exit.exception.code, 0)
+
+        assert mock_file.called is False
+        assert mock_run.called
 
     @patch("runpod.serverless.modules.rp_local.run_job", return_value={})
     @patch("builtins.open", new_callable=mock_open, read_data='{"input": "test"}')
@@ -38,9 +41,12 @@ class TestRunLocal(unittest.TestCase):
             "rp_args": {}
         }
         with patch("os.path.exists", return_value=True):
-            with self.assertRaises(SystemExit) as cm:
+            with self.assertRaises(SystemExit) as sys_exit:
                 rp_local.run_local(config)
-            self.assertEqual(cm.exception.code, 0)
+            self.assertEqual(sys_exit.exception.code, 0)
+
+        assert mock_file.called
+        assert mock_run.called
 
     @patch("runpod.serverless.modules.rp_local.run_job", return_value={"error": "test_error"})
     @patch("builtins.open", new_callable=mock_open, read_data='{"input": "test"}')
@@ -57,9 +63,12 @@ class TestRunLocal(unittest.TestCase):
                 }
             }
         }
-        with self.assertRaises(SystemExit) as cm:
+        with self.assertRaises(SystemExit) as sys_exit:
             rp_local.run_local(config)
-        self.assertEqual(cm.exception.code, 1)
+        self.assertEqual(sys_exit.exception.code, 1)
+
+        assert mock_file.called is False
+        assert mock_run.called
 
     def test_run_local_without_test_input_json(self):
         '''
@@ -70,9 +79,9 @@ class TestRunLocal(unittest.TestCase):
             "rp_args": {}
         }
         with patch("os.path.exists", return_value=False):
-            with self.assertRaises(SystemExit) as cm:
+            with self.assertRaises(SystemExit) as sys_exit:
                 rp_local.run_local(config)
-            self.assertEqual(cm.exception.code, 1)
+            self.assertEqual(sys_exit.exception.code, 1)
 
     @patch("runpod.serverless.modules.rp_local.run_job", return_value={})
     @patch("builtins.open", new_callable=mock_open, read_data='{"not_input": "test"}')
@@ -85,6 +94,9 @@ class TestRunLocal(unittest.TestCase):
             "rp_args": {}
         }
         with patch("os.path.exists", return_value=True):
-            with self.assertRaises(SystemExit) as cm:
+            with self.assertRaises(SystemExit) as sys_exit:
                 rp_local.run_local(config)
-            self.assertEqual(cm.exception.code, 1)
+            self.assertEqual(sys_exit.exception.code, 1)
+
+        assert mock_file.called
+        assert mock_run.called is False
