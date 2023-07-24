35155d8b55c81cb29d8eff89616ed40a6a524146
Jorg Doku <jd728@cornell.edu>
2023-07-23 23:42:54 -0500

    finish refactor

--- runpod/serverless/worker.py
+++ runpod/serverless/worker.py
@@ -46,110 +46,186 @@ def _is_local(config) -> bool:
     return False
 
 
-# Constants
-SCALE_FACTOR = 2
-INITIAL_CONCURRENT_REQUESTS = 1
-MAX_CONCURRENT_REQUESTS = 100
-MIN_CONCURRENT_REQUESTS = 1
-AVAILABILITY_RATIO_THRESHOLD = 0.90
+class JobProcessor():
+    """
+    A class for automatically retrieving new jobs from the server and processing them concurrently.
 
-class MultiProcessingEngine():
+    Attributes:
+        server_url (str): The URL of the server to retrieve jobs from.
+        max_concurrent_jobs (int): The maximum number of jobs to process concurrently.
+        upscale_factor (float): The factor by which to upscale the job retrieval rate.
+        downscale_factor (float): The factor by which to downscale the job retrieval rate.
+
+    Methods:
+        get_jobs() -> List[Dict]:
+            Retrieves multiple jobs from the server in parallel using concurrent requests.
+
+        upscale_rate() -> None:
+            Upscales the job retrieval rate by adjusting the number of concurrent requests.
+
+        downscale_rate() -> None:
+            Downscales the job retrieval rate by adjusting the number of concurrent requests.
+
+        rescale_request_rate() -> None:
+            Rescales the job retrieval rate based on factors such as job queue availability 
+            and handler utilization.
+
+    Usage example:
+        job_processor = JobProcessor(config)
+
+        # Retrieving multiple jobs in parallel
+        jobs_list = job_processor.get_jobs(session)
+
+        # Upscaling the rate for faster job retrieval
+        job_processor.upscale_rate()
+
+        # Downscaling the rate for more conservative job retrieval
+        job_processor.downscale_rate()
+
+        # Rescaling based on the queue, availability, and other metrics
+        job_processor.rescale_request_rate()
     """
-    Automatically start concurrently retrieving jobs from SLS, including the ability 
-    to scale up or scale down the rate at which we're retrieving jobs. Additionally, 
-    we separate job retrieval from job execution in order to decouple the latency as 
-    much as possible between the two phases.
-    """
-    def __init__(self, config):
-        self.background_tasks = set()
-        self.num_concurrent_requests = INITIAL_CONCURRENT_REQUESTS
-        self.job_history = []  # Keeps track of recent job results
-        self.handler_fully_utilized = config.get('handler_fully_utilized')
-        self.kill_worker = False
+
+    # Scaling Constants
+    CONCURRENCY_SCALE_FACTOR = 2
+    AVAILABILITY_RATIO_THRESHOLD = 0.90
+    INITIAL_CONCURRENT_REQUESTS = 1
+    MAX_CONCURRENT_REQUESTS = 100
+    MIN_CONCURRENT_REQUESTS = 1
+
+    def __init__(self, handler_fully_utilized):
+        self.background_get_job_tasks = set()
+        self.num_concurrent_get_job_requests = JobProcessor.INITIAL_CONCURRENT_REQUESTS
+        self.job_history = []
+        self.handler_fully_utilized = handler_fully_utilized
+        self.is_alive = True
+
+    def kill_worker(self):
+        """
+        Whether to kill the worker.
+        """
+        self.is_alive = False
 
     async def get_jobs(self, session):
         """
-        Retrieve 'num_concurrent_requests' number of jobs from SLS, 
-        performing sleep, and rescaling appropriately.
+        Retrieve multiple jobs from the server in parallel using concurrent requests.
+
+        Returns:
+            List[Any]: A list of job data retrieved from the server.
         """
         while True:
             tasks = [
                 asyncio.create_task(
                     get_job(session, retry=False)
-                ) for _ in range(self.num_concurrent_requests)]
+                ) for _ in range(self.num_concurrent_get_job_requests)]
+
             for job_future in asyncio.as_completed(tasks):
                 job = await job_future
-                self.job_history.append(job)
+                self.job_history.append(1 if job else 0)
 
+                # Add the job to our list of jobs
                 if job:
                     job_list.add_job(job["id"])
                     log.debug(f"{job['id']} | Set Job ID")
                     yield job
 
-            # We retrieve num_concurrent_requests jobs per second.
-            await asyncio.sleep(1)
+            # During the single processing scenario, wait for the job to finish processing.
+            if not self.handler_fully_utilized:
+                await asyncio.wait(self.background_get_job_tasks)
+                break
+            else:
+                # We retrieve num_concurrent_get_job_requests jobs per second.
+                await asyncio.sleep(1)
 
-            # Rescale the retrieval rate appropriately.
-            self.rescale_request_rate()
+                # Rescale the retrieval rate appropriately.
+                self.rescale_request_rate()
 
-            log.info(
-                f"Concurrent Get Jobs | The number of concurrent get_jobs is "
-                f"{self.num_concurrent_requests}."
-            )
+                # Show logs
+                log.info(
+                    f"Concurrent Get Jobs | The number of concurrent get_jobs is "
+                    f"{self.num_concurrent_get_job_requests}."
+                )
 
-    def rescale_request_rate(self, force_downscale=False) -> None:
+    def upscale_rate(self) -> None:
+        """
+        Upscale the job retrieval rate by adjusting the number of concurrent requests.
+
+        This method increases the number of concurrent requests to the server,
+        effectively retrieving more jobs per unit of time.
+        """
+        self.num_concurrent_get_job_requests = min(
+            self.num_concurrent_get_job_requests *
+            JobProcessor.CONCURRENCY_SCALE_FACTOR,
+            JobProcessor.MAX_CONCURRENT_REQUESTS
+        )
+
+    def downscale_rate(self) -> None:
+        """
+        Downscale the job retrieval rate by adjusting the number of concurrent requests.
+
+        This method decreases the number of concurrent requests to the server,
+        effectively retrieving fewer jobs per unit of time.
+        """
+        self.num_concurrent_get_job_requests = int(max(
+            self.num_concurrent_get_job_requests // JobProcessor.CONCURRENCY_SCALE_FACTOR,
+            JobProcessor.MIN_CONCURRENT_REQUESTS
+        ))
+
+    def rescale_request_rate(self) -> None:
         """
         Scale up or down the rate at which we are handling jobs from SLS.
         """
-        if force_downscale:
-            self.num_concurrent_requests = int(max(
-                self.num_concurrent_requests // SCALE_FACTOR, MIN_CONCURRENT_REQUESTS))
+        # If we're inside the single-processing setting.
+        if not self.handler_fully_utilized:
             return
 
+        # There's no job history for our rescaling mechanism.
         if len(self.job_history) == 0:
             return
 
         # Compute the availability ratio of the job queue.
-        none_jobs_count = sum(job is None for job in self.job_history)
-        availability_ratio = 1 - none_jobs_count / len(self.job_history)
+        availability_ratio = sum(self.job_history) / len(self.job_history)
 
         # If our worker is fully utilized or the SLS queue is throttling, reduce the job query rate.
         if self.handler_fully_utilized() is True:
-            log.debug("The handler is fully utilized. Downscaling now.")
+            log.debug("Reducing job query rate due to full worker utilization.")
 
-            # Reduce job query rate.
-            self.num_concurrent_requests = int(max(
-                self.num_concurrent_requests // SCALE_FACTOR, MIN_CONCURRENT_REQUESTS))
-        elif availability_ratio < 1 / SCALE_FACTOR:
-            # Reduce job query rate.
-            self.num_concurrent_requests = int(max(
-                self.num_concurrent_requests // SCALE_FACTOR, MIN_CONCURRENT_REQUESTS))
-        elif availability_ratio >= AVAILABILITY_RATIO_THRESHOLD:
-            # Assess if SLS queue has enough jobs to scale job query rate.
-            # Increase concurrent request count.
-            self.num_concurrent_requests = min(
-                self.num_concurrent_requests * SCALE_FACTOR, MAX_CONCURRENT_REQUESTS
-            )
+            self.downscale_rate()
+        elif availability_ratio < 1 / JobProcessor.CONCURRENCY_SCALE_FACTOR:
+            log.debug("Reducing job query rate due to low job queue availability.")
+
+            self.downscale_rate()
+        elif availability_ratio >= JobProcessor.AVAILABILITY_RATIO_THRESHOLD:
+            log.debug("Increasing job query rate due to increased job queue availability.")
+
+            self.upscale_rate()
 
         # Clear the job history
         self.job_history.clear()
 
 
 # ------------------------- Main Worker Running Loop ------------------------- #
-async def run_worker_multi(config: Dict[str, Any]) -> None:
+async def run_worker(config: Dict[str, Any]) -> None:
     """
     Starts the worker loop for multi-processing.
+
+    Args:
+        config (Dict[str, Any]): Configuration parameters for the worker.
     """
     auth_header = _get_auth_header()
     connector = aiohttp.TCPConnector(limit=None)
-    multi_processing_engine = MultiProcessingEngine(config=config)
+
+    job_processor = JobProcessor(
+        handler_fully_utilized=config.get('handler_fully_utilized')
+    )
+
     async with aiohttp.ClientSession(
-        connector=connector, headers=auth_header, timeout=_TIMEOUT) as session:
+            connector=connector, headers=auth_header, timeout=_TIMEOUT) as session:
 
         heartbeat.start_ping()
 
-        multi_processing_engine.kill_worker = False # Flag to kill the worker after job is complete.
-        while multi_processing_engine.kill_worker is False:
+        # Flag to kill the worker after job is complete.
+        while job_processor.is_alive:
             async def process_job(job):
                 if inspect.isgeneratorfunction(config["handler"]):
                     job_result = run_job_generator(config["handler"], job)
@@ -163,85 +239,41 @@ async def run_worker_multi(config: Dict[str, Any]) -> None:
 
                 # If refresh_worker is set, pod will be reset after job is complete.
                 if config.get("refresh_worker", False):
-                    log.info(f"refresh_worker | Flag set, stopping pod after job {job['id']}.")
+                    log.info(
+                        f"refresh_worker | Flag set, stopping pod after job {job['id']}.")
                     job_result["stopPod"] = True
-                    multi_processing_engine.kill_worker = True
+                    job_processor.kill_worker()
 
                 # If rp_debugger is set, debugger output will be returned.
                 if config["rp_args"].get("rp_debugger", False) and isinstance(job_result, dict):
                     log.debug("rp_debugger | Flag set, return debugger output.")
                     job_result["output"]["rp_debugger"] = rp_debugger.get_debugger_output()
 
-                    ready_delay = (config["reference_counter_start"] - REF_COUNT_ZERO) * 1000
+                    # Calculate ready delay for the debugger output.
+                    ready_delay = (
+                        config["reference_counter_start"] - REF_COUNT_ZERO) * 1000
                     job_result["output"]["rp_debugger"]["ready_delay_ms"] = ready_delay
                 else:
-                    log.debug("rp_debugger | Flag not set, skipping debugger output.")
+                    log.debug(
+                        "rp_debugger | Flag not set, skipping debugger output.")
+
                     rp_debugger.clear_debugger_output()
 
+                # Send the job result to SLS
                 await send_result(session, job_result, job)
 
                 log.info(f'{job["id"]} | Finished')
                 job_list.remove_job(job["id"])
 
-            # Create process job task
-            async for job in multi_processing_engine.get_jobs(session):
+            async for job in job_processor.get_jobs(session):
                 # Process the job here
                 task = asyncio.create_task(process_job(job))
-                multi_processing_engine.background_tasks.add(task)
-                task.add_done_callback(multi_processing_engine.background_tasks.discard)
+                job_processor.background_get_job_tasks.add(task)
+                task.add_done_callback(
+                    job_processor.background_get_job_tasks.discard)
 
-        asyncio.get_event_loop().stop() # Stops the worker loop if the kill_worker flag is set.
-
-
-async def run_worker(config: Dict[str, Any]) -> None:
-    """
-    Starts the worker loop.
-    """
-    auth_header = _get_auth_header()
-    async with aiohttp.ClientSession(headers=auth_header, timeout=_TIMEOUT) as session:
-
-        heartbeat.start_ping()
-
-        kill_worker = False # Flag to kill the worker after job is complete.
-        while kill_worker is False:
-            job = await get_job(session)
-
-            job_list.add_job(job["id"])
-            log.debug(f"{job['id']} | Set Job ID")
-
-            if inspect.isgeneratorfunction(config["handler"]):
-                job_result = run_job_generator(config["handler"], job)
-
-                log.debug("Handler is a generator, streaming results.")
-                async for job_stream in job_result:
-                    await stream_result(session, job_stream, job)
-                job_result = {}
-            else:
-                job_result = await run_job(config["handler"], job)
-
-            # If refresh_worker is set, pod will be reset after job is complete.
-            if config.get("refresh_worker", False):
-                log.info(f"refresh_worker | Flag set, stopping pod after job {job['id']}.")
-                job_result["stopPod"] = True
-                kill_worker = True
-
-            # If rp_debugger is set, debugger output will be returned.
-            if config["rp_args"].get("rp_debugger", False) and isinstance(job_result, dict):
-                log.debug("rp_debugger | Flag set, return debugger output.")
-                job_result["output"]["rp_debugger"] = rp_debugger.get_debugger_output()
-
-                ready_delay = (config["reference_counter_start"] - REF_COUNT_ZERO) * 1000
-                job_result["output"]["rp_debugger"]["ready_delay_ms"] = ready_delay
-            else:
-                log.debug("rp_debugger | Flag not set, skipping debugger output.")
-                rp_debugger.clear_debugger_output()
-
-            await send_result(session, job_result, job)
-
-            log.info(f'{job["id"]} | Finished')
-            job_list.remove_job(job["id"])
-
-        asyncio.get_event_loop().stop() # Stops the worker loop if the kill_worker flag is set.
+        # Stops the worker loop if the kill_worker flag is set.
+        asyncio.get_event_loop().stop()
 
 
 def main(config: Dict[str, Any]) -> None:
@@ -255,13 +287,11 @@ def main(config: Dict[str, Any]) -> None:
     else:
         try:
             work_loop = asyncio.new_event_loop()
-            if config.get('handler_fully_utilized'):
-                asyncio.ensure_future(run_worker_multi(config), loop=work_loop)
-            else:
-                asyncio.ensure_future(run_worker(config), loop=work_loop)
+            asyncio.ensure_future(run_worker(config), loop=work_loop)
             work_loop.run_forever()
 
-        except Exception as exception: # pylint: disable=broad-exception-caught
-            log.debug(f"rp_debugger | The event loop has closed due to {exception}.")
+        except Exception as exception:  # pylint: disable=broad-exception-caught
+            log.debug(
+                f"rp_debugger | The event loop has closed due to {exception}.")
         finally:
             work_loop.close()
