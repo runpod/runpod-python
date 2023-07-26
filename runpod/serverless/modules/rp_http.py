052088ebd7bcd51906a19c4fc000c8302a1dbfa8
Jorg Doku <jd728@cornell.edu>
2023-07-26 10:54:27 -0500

    http streaming

--- runpod/serverless/modules/rp_http.py
+++ runpod/serverless/modules/rp_http.py
@@ -13,6 +13,9 @@ from .worker_state import WORKER_ID
 JOB_DONE_URL_TEMPLATE = str(os.environ.get('RUNPOD_WEBHOOK_POST_OUTPUT'))
 JOB_DONE_URL_TEMPLATE = JOB_DONE_URL_TEMPLATE.replace('$RUNPOD_POD_ID', WORKER_ID)
 
+JOB_STREAM_URL_TEMPLATE = str(os.environ.get('RUNPOD_WEBHOOK_POST_STREAM'))
+JOB_STREAM_URL_TEMPLATE = JOB_STREAM_URL_TEMPLATE.replace('$RUNPOD_POD_ID', WORKER_ID)
+
 log = RunPodLogger()
 job_list = Jobs()
 
@@ -60,7 +63,7 @@ async def stream_result(session, job_data, job):
     '''
     try:
         job_data = json.dumps(job_data, ensure_ascii=False)
-        job_done_url = JOB_DONE_URL_TEMPLATE.replace('$ID', job['id'])
+        job_done_url = JOB_STREAM_URL_TEMPLATE.replace('$ID', job['id'])
 
         await transmit(session, job_data, job_done_url)
 
