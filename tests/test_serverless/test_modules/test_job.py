b9a0047cdbfec2d7683fcaf7d289a3731a86fd1a
Justin Merrell <merrelljustin@gmail.com>
2023-07-29 10:00:34 -0400

    Change print out and fixed tests

--- tests/test_serverless/test_modules/test_job.py
+++ tests/test_serverless/test_modules/test_job.py
@@ -102,7 +102,7 @@ class TestJob(IsolatedAsyncioTestCase):
             job = await rp_job.get_job(mock_session, retry=False)
 
             assert job is None
-            assert mock_log.error.call_count == 2
+            assert mock_log.error.call_count == 1
 
     @pytest.mark.asyncio
     async def test_get_job_no_input(self):
