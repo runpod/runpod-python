d6b8d7e584dd311f2ce3b8532c41cfbd95ec3a3f
Justin Merrell <merrelljustin@gmail.com>
2023-07-31 13:40:52 -0400

    Ping and log cleanup

--- runpod/serverless/modules/rp_http.py
+++ runpod/serverless/modules/rp_http.py
@@ -30,14 +30,13 @@ async def transmit(session, job_data, url):
         "Content-Type": "application/x-www-form-urlencoded"
     }
 
-    log.debug(f"Initiating result API call to {url} with job data: {job_data}")
     async with session.post(url,
                             data=job_data,
                             headers=headers,
                             raise_for_status=True) as resp:
-        result = await resp.text()
+        await resp.text()
 
-    log.debug(f"Completed result API call to {url}. Response: {result}")
+    log.debug(f"{job_data['id']} | Transmitted result.")
 
 
 async def send_result(session, job_data, job):
@@ -56,6 +55,7 @@ async def send_result(session, job_data, job):
 
     except Exception as err:  # pylint: disable=broad-except
         log.error(f"Error while returning job result {job['id']}: {err}")
+
 
 async def stream_result(session, job_data, job):
     '''
