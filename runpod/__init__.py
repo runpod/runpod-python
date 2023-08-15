46f942c2b56ed76a1017cb53f893e532007be6ff
Vyacheslav Alexeev <alexeev.corp@gmail.com>
2023-08-15 17:47:32 +0200

    hotfix

--- runpod/__init__.py
+++ runpod/__init__.py
@@ -7,7 +7,7 @@ from .endpoint import Endpoint
 from .endpoint import AsyncioEndpoint, AsyncioJob
 from .api_wrapper.ctl_commands import(
     get_gpus, get_gpu,
-    get_pod,
+    get_pods,
     create_pod, stop_pod, resume_pod, terminate_pod
 )
 from .cli.config import set_credentials, check_credentials, get_credentials
