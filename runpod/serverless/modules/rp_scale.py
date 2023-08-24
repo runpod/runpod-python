6742801c82d70cd5ce8d9c59afe822e2e9b06033
Jorg Doku <jd728@cornell.edu>
2023-08-24 16:07:16 -0500

    fix off main

--- runpod/serverless/modules/rp_scale.py
+++ runpod/serverless/modules/rp_scale.py
@@ -4,7 +4,11 @@ Provides the functionality for scaling the runpod serverless worker.
 '''
 
 import asyncio
+import threading
 import typing
+
+import os
+import aiohttp
 
 from runpod.serverless.modules.rp_logger import RunPodLogger
 from .rp_job import get_job
@@ -12,6 +16,7 @@ from .worker_state import Jobs
 
 log = RunPodLogger()
 job_list = Jobs()
+
 
 class JobScaler():
     """
@@ -54,19 +59,20 @@ class JobScaler():
     """
 
     # Scaling Constants
-    CONCURRENCY_SCALE_FACTOR = 2
+    CONCURRENCY_SCALE_FACTOR = 2.0
     AVAILABILITY_RATIO_THRESHOLD = 0.90
     INITIAL_CONCURRENT_REQUESTS = 1
     MAX_CONCURRENT_REQUESTS = 100
     MIN_CONCURRENT_REQUESTS = 1
-    SLEEP_INTERVAL_SEC = 1
+    SLEEP_INTERVAL_SEC = 0.30
 
-    def __init__(self, concurrency_controller: typing.Any):
+    def __init__(self, concurrency_controller: typing.Any = None):
         self.background_get_job_tasks = set()
         self.num_concurrent_get_job_requests = JobScaler.INITIAL_CONCURRENT_REQUESTS
         self.job_history = []
         self.concurrency_controller = concurrency_controller
         self._is_alive = True
+        self.queue = []
 
     def is_alive(self):
         """
@@ -87,48 +93,110 @@ class JobScaler():
         self.background_get_job_tasks.add(task)
         task.add_done_callback(self.background_get_job_tasks.discard)
 
-    async def get_jobs(self, session):
+    def start(self):
+        """
+        empty
+        """
+        loop = asyncio.new_event_loop()
+        threading.Thread(
+            target=lambda: loop.run_until_complete(self.get_jobs()),
+            daemon=True
+        ).start()
+
+    def get_from_queue(self):
+        """
+        Retrieve jobs from the job take scaler queue.
+        """
+        # Retrieve a snapshot of the queue.
+        snapshot = self.queue.copy()
+
+        # Clear the snapshot from the queue.
+        self.queue[0:len(snapshot)] = []
+
+        return snapshot
+
+    async def get_jobs(self):
         """
         Retrieve multiple jobs from the server in parallel using concurrent requests.
 
         Returns:
             List[Any]: A list of job data retrieved from the server.
         """
-        while True:
-            # Finish if the job_scale is not alive
-            if not self.is_alive():
-                break
+        # A session needs to be instantiated within the 'get_jobs' coroutine.
+        timeout = aiohttp.ClientTimeout(total=300, connect=2, sock_connect=2)
+        connector = aiohttp.TCPConnector(limit=None, limit_per_host=None)
+        session = aiohttp.ClientSession(
+            connector=connector,
+            headers={
+                "Authorization": f"{os.environ.get('RUNPOD_AI_API_KEY')}"},
+            timeout=timeout
+        )
 
-            for _ in range(self.num_concurrent_get_job_requests):
-                job = await get_job(session, retry=False)
-                self.job_history.append(1 if job else 0)
-                if job:
-                    yield job
+        while self.is_alive():
+            # Employ parallel processing if there are jobs in progress.
+            use_parallel_processing = job_list.get_job_list() is not None
 
-            # During the single processing scenario, wait for the job to finish processing.
+            # We intend to maintain the 'jobs_in_progress' value constant throughout the entire
+            # parallel processing flow below to prevent a race condition within SLS.
+            # If the 'jobs_in_progress' count is 0, we should proceed sequentially.
+            # Otherwise, we should proceed in parallel.
+            if use_parallel_processing:
+                # Prepare the 'get_job' tasks for parallel execution.
+                tasks = [
+                    asyncio.create_task(
+                        get_job(session, force_in_progress=True, retry=False)
+                    )
+                    for _ in range(self.num_concurrent_get_job_requests)
+                ]
+
+                # Wait for all the 'get_job' tasks, which are running in parallel, to be completed.
+                for job_future in asyncio.as_completed(tasks):
+                    job = await job_future
+                    self.job_history.append(1 if job else 0)
+
+                    if job:
+                        self.queue.append(job)
+            else:
+                for _ in range(self.num_concurrent_get_job_requests):
+                    # The latency for get_job is 0.3 seconds.
+                    job = await get_job(session, retry=False)
+                    self.job_history.append(1 if job else 0)
+
+                    if job:
+                        self.queue.append(job)
+
+            # In the scenario involving a single processing worker, we employ a variant of the
+            # concurrency_controller in which we wait or delay until the tasks have been fully
+            # completed. For instance, it is plausible to encounter a worker handling CPU-intensive
+            # workloads. In such workloads, tasks may range from completing within 10ms to 100ms on
+            # a single worker. Therefore, it becomes logical to enqueue multiple jobs simultaneously
+            # within the JobScaler to manage these workloads effectively.
             if self.concurrency_controller is None:
                 # Create a copy of the background job tasks list to keep references to the tasks.
-                job_tasks_copy = self.background_get_job_tasks.copy()
-                if job_tasks_copy:
-                    # Wait for the job tasks to finish processing before continuing.
-                    await asyncio.wait(job_tasks_copy)
-                # Exit the loop after processing a single job (since the handler is fully utilized).
-                await asyncio.sleep(JobScaler.SLEEP_INTERVAL_SEC)
+                # Wait for the job tasks to finish processing before continuing.
+                # Note: asyncio.wait requires a non-empty list or it throws an exception.
+                jobs = self.background_get_job_tasks.copy()
+                if jobs:
+                    await asyncio.wait(jobs)
                 break
-
-            # We retrieve num_concurrent_get_job_requests jobs per second.
-            await asyncio.sleep(JobScaler.SLEEP_INTERVAL_SEC)
-
-            # Rescale the retrieval rate appropriately.
-            self.rescale_request_rate()
 
             # Show logs
             log.info(
                 f"Concurrent Get Jobs | The number of concurrent get_jobs is "
                 f"{self.num_concurrent_get_job_requests}."
+                " The number of yielded jobs is "
+                f"{sum(self.job_history)} of {len(self.job_history)}."
             )
 
+            # Adjust the job retrieval rate by considering factors such as queue availability
+            # and the pace at which we are processing jobs within the multi-processing worker.
+            self.rescale_request_rate()
 
+            # In the parallel processing scenario, we intend to sleep for a certain number of
+            # seconds at each interval. However, in the case of sequential processing, this results
+            # in excessive overhead, which we strive to avoid.
+            if use_parallel_processing:
+                await asyncio.sleep(JobScaler.SLEEP_INTERVAL_SEC)
 
     def upscale_rate(self) -> None:
         """
@@ -137,11 +205,11 @@ class JobScaler():
         This method increases the number of concurrent requests to the server,
         effectively retrieving more jobs per unit of time.
         """
-        self.num_concurrent_get_job_requests = min(
+        self.num_concurrent_get_job_requests = int(min(
             self.num_concurrent_get_job_requests *
             JobScaler.CONCURRENCY_SCALE_FACTOR,
             JobScaler.MAX_CONCURRENT_REQUESTS
-        )
+        ))
 
     def downscale_rate(self) -> None:
         """
@@ -160,10 +228,11 @@ class JobScaler():
         Scale up or down the rate at which we are handling jobs from SLS.
         """
         # Compute the availability ratio of the job queue.
-        availability_ratio = sum(self.job_history) / len(self.job_history)
+        availability_ratio = sum(self.job_history) / \
+            (len(self.job_history) + 0.0)
 
         # If our worker is fully utilized or the SLS queue is throttling, reduce the job query rate.
-        if self.concurrency_controller() is True:
+        if self.concurrency_controller and self.concurrency_controller() is True:
             log.debug("Reducing job query rate due to full worker utilization.")
 
             self.downscale_rate()
--- runpod/serverless/worker.py
+++ runpod/serverless/worker.py
@@ -92,29 +92,35 @@ async def run_worker(config: Dict[str, Any]) -> None:
     Args:
         config (Dict[str, Any]): Configuration parameters for the worker.
     """
+
     heartbeat.start_ping()
 
-    client_session = aiohttp.ClientSession(
+    async with aiohttp.ClientSession(
         connector=aiohttp.TCPConnector(limit=None),
-        headers=_get_auth_header(), timeout=_TIMEOUT
-    )
-
-    async with client_session as session:
+        headers=_get_auth_header(),
+        timeout=_TIMEOUT
+    ) as session:
+        # Create the job scaler, which retrieves jobs from the RunPod queue.
         job_scaler = JobScaler(
             concurrency_controller=config.get('concurrency_controller', None)
         )
 
-        while job_scaler.is_alive():
+        # Begin taking jobs in the background using the job-take scaling mechanism.
+        job_scaler.start()
 
-            async for job in job_scaler.get_jobs(session):
+        while job_scaler.is_alive():
+            for job in job_scaler.get_from_queue():
                 # Process the job here
                 task = asyncio.create_task(_process_job(job, session, job_scaler, config))
 
                 # Track the task
                 job_scaler.track_task(task)
 
-                # Allow job processing
+                # Allow other tasks to make progress.
                 await asyncio.sleep(0)
+
+            # Allow other tasks to make progress.
+            await asyncio.sleep(0)
 
         # Stops the worker loop if the kill_worker flag is set.
         asyncio.get_event_loop().stop()
