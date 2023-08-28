ef0ce1f572b216d4f9645f006a0ea27cb2ecd267
Justin Merrell <merrelljustin@gmail.com>
2023-08-28 17:34:59 -0400

    Update test_asyncio_runner.py

--- tests/test_endpoint/test_asyncio_runner.py
+++ tests/test_endpoint/test_asyncio_runner.py
@@ -1,7 +1,6 @@
 ''' Unit tests for the asyncio_runner module. '''
 # pylint: disable=too-few-public-methods
 
-
 import tracemalloc
 import asyncio
 import unittest
@@ -68,6 +67,31 @@ class TestJob(IsolatedAsyncioTestCase):
             cancel_result = await job.cancel()
             assert cancel_result == {"result": "CANCELLED"}
 
+    async def test_output_in_progress_then_completed(self):
+        '''Tests Job.output when status is initially IN_PROGRESS and then changes to COMPLETED'''
+        with (
+            patch("runpod.endpoint.asyncio.asyncio_runner.asyncio.sleep") as mock_sleep,
+            patch("aiohttp.ClientSession") as mock_session
+        ):
+            mock_resp = MagicMock()
+            responses = [
+                {"status": "IN_PROGRESS"},
+                {"status": "COMPLETED"},
+                {"output": "OUTPUT"}
+            ]
+
+            async def json_side_effect():
+                if responses:
+                    return responses.pop(0)
+                return {"status": "IN_PROGRESS"}
+
+            mock_resp.json = json_side_effect
+            mock_session.get.return_value.__aenter__.return_value = mock_resp
+
+            job = Job("endpoint_id", "job_id", mock_session)
+            output = await job.output()
+            assert output == "OUTPUT"
+
 class TestEndpoint:
     ''' Unit tests for the Endpoint class. '''
 
@@ -84,6 +108,17 @@ class TestEndpoint:
             endpoint = Endpoint("endpoint_id", mock_session)
             job = await endpoint.run({"input": "INPUT"})
             assert job.job_id == "job_id"
+
+class TestEndpointInitialization(unittest.TestCase):
+    '''Tests for the Endpoint class initialization.'''
+
+    def test_endpoint_initialization(self):
+        '''Tests initialization of Endpoint class.'''
+        with patch("aiohttp.ClientSession"):
+            endpoint = Endpoint("endpoint_id", MagicMock())
+            self.assertEqual(endpoint.endpoint_url, "https://api.runpod.ai/v2/endpoint_id/run")
+            self.assertIn("Content-Type", endpoint.headers)
+            self.assertIn("Authorization", endpoint.headers)
 
 if __name__ == "__main__":
     unittest.main()
