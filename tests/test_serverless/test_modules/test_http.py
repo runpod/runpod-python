90afc9d3e705ec2c6e6c16da1a817fdada6407d4
Justin Merrell <merrelljustin@gmail.com>
2023-08-29 12:53:42 -0400

    Update test_http.py

--- tests/test_serverless/test_modules/test_http.py
+++ tests/test_serverless/test_modules/test_http.py
@@ -41,7 +41,7 @@ class TestHTTP(unittest.IsolatedAsyncioTestCase):
 
             mock_retry.return_value.post.assert_called_with(
                 'JOB_DONE_URL',
-                data=f'{json.dumps(self.job_data, ensure_ascii=False)}',
+                data=str(json.dumps(self.job_data, ensure_ascii=False)),
                 headers={
                     "charset": "utf-8",
                     "Content-Type": "application/x-www-form-urlencoded"
@@ -65,6 +65,13 @@ class TestHTTP(unittest.IsolatedAsyncioTestCase):
                 message="Error message"
             )
 
+            # mock_retry raise ClientResponseError
+            mock_retry.post.return_value = aiohttp.ClientResponseError(
+                request_info=None,
+                history=None,
+                status=500,
+                message="Error message"
+            )
 
             mock_jobs.return_value = set(['test_id'])
             send_return_local = await rp_http.send_result(AsyncMock(), self.job_data, self.job)
