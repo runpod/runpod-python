fbc1ca3db41e2bb73474d48956b40e933b146616
Justin Merrell <merrelljustin@gmail.com>
2023-08-29 16:57:27 -0400

    Update rp_http.py

--- runpod/serverless/modules/rp_http.py
+++ runpod/serverless/modules/rp_http.py
@@ -48,7 +48,7 @@ async def _handle_result(session, job_data, job, url_template, log_message):
         await _transmit(session, url, serialized_job_data)
         log.debug(f"{job['id']} | {log_message}")
 
-    except aiohttp.ClientResponseError as err:
+    except aiohttp.ClientError as err:
         log.error(f"{job['id']} | Client response error while transmitting job. | {err}")
 
     except (TypeError, RuntimeError) as err:
