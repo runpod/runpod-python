8f09971ac18dffcdf9ee92f48c244940e974f204
Justin Merrell <merrelljustin@gmail.com>
2023-08-29 09:23:58 -0400

    Update test_http.py

--- tests/test_serverless/test_modules/test_http.py
+++ tests/test_serverless/test_modules/test_http.py
@@ -38,9 +38,8 @@ class TestHTTP(unittest.IsolatedAsyncioTestCase):
              patch('runpod.serverless.modules.rp_http.job_list.jobs') as mock_jobs:
 
             mock_jobs.return_value = set(['test_id'])
-            send_return_local = await rp_http.send_result(mock_session, self.job_data, self.job)
+            await rp_http.send_result(mock_session, self.job_data, self.job)
 
-            assert send_return_local is None
             assert mock_log.debug.call_count == 0
             assert mock_log.error.call_count == 0
             assert mock_log.info.call_count == 0
