0b7bc63294bde3fe2cc020c8bb17bef9fec503d1
Justin Merrell <merrelljustin@gmail.com>
2023-08-30 13:45:11 -0400

    feat: connect to pod

--- runpod/cli/pod/commands.py
+++ runpod/cli/pod/commands.py
@@ -41,9 +41,18 @@ def create_new_pod(name, image, gpu_type, gpu_count, support_public_ip, template
         image = 'runpod/pytorch:2.0.1-py3.10-cuda11.8.0-devel'
         gpu_type = 'NVIDIA GeForce RTX 3090'
         support_public_ip = True
+        ports ='22/tcp'
 
         click.echo('Launching default pod...')
 
-    new_pod = create_pod(name, image, gpu_type, support_public_ip=support_public_ip)
+    new_pod = create_pod(name, image, gpu_type, support_public_ip=support_public_ip, ports=ports)
 
     click.echo(f'Pod {new_pod["id"]} has been created.')
+
+@pod_cli.command('connect')
+@click.argument('pod_id')
+def connect_to_pod(pod_id):
+    '''
+    Connects to a pod.
+    '''
+    click.echo(f'Connecting to pod {pod_id}...')
--- runpod/cli/pod/functions.py
+++ runpod/cli/pod/functions.py
@@ -1,3 +1,30 @@
 '''
 RunPod | CLI | Pod | Functions
 '''
+import os
+import subprocess
+
+from runpod import get_pod
+
+SSH_KEY_FOLDER = os.path.expanduser('~/.runpod/ssh')
+
+def open_ssh_connection(pod_id):
+    '''
+    Opens an SSH connection to a pod.
+    '''
+    pod = get_pod(pod_id)
+
+    if pod['desiredStatus'] == 'running':
+            for port in pod['runtime']['ports']:
+                if port['privatePort'] == 22:
+                    pod_ip = port['ip']
+                    pod_port = port['publicPort']
+
+    key_files = [f for f in os.listdir(SSH_KEY_FOLDER) if os.path.isfile(os.path.join(SSH_KEY_FOLDER, f))]
+
+    cmd = ["ssh" , "-p", str(pod_port)]
+    for key_file in key_files:
+        cmd.extend(["-i", os.path.join(SSH_KEY_FOLDER, key_file)])
+    cmd.append(f"root@{pod_ip}")
+
+    subprocess.run(cmd)
