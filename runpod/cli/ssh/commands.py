9dc8abebb181c8dbd49a72a2eca982b96153c60f
Justin Merrell <merrelljustin@gmail.com>
2023-09-01 18:35:17 -0400

    Update commands.py

--- runpod/cli/ssh/commands.py
+++ runpod/cli/ssh/commands.py
@@ -8,7 +8,7 @@ from .functions import get_user_pub_keys, generate_ssh_key_pair
 
 @click.group('ssh')
 def ssh_cli():
-    '''A collection of CLI functions for SSH.'''
+    '''Manage and configure SSH keys.'''
 
 @ssh_cli.command('list-keys')
 def list_keys():
@@ -16,12 +16,9 @@ def list_keys():
     Lists the SSH keys for the current user.
     '''
     key_list = get_user_pub_keys()
-
     table = PrettyTable(['Public Key', 'Type', 'Fingerprint'])
-
     for key in key_list:
         table.add_row((key['name'], key['type'], key['fingerprint']))
-
     click.echo(table)
 
 @ssh_cli.command('add-key')
@@ -32,10 +29,10 @@ def add_key(key, key_file):
     Adds an SSH key to the current user account.
     If no key is provided, one will be generated.
     '''
-    click.confirm('Would you like to add an SSH key to your account?', abort=True)
-    key_name = click.prompt('Please enter a name for this key', default='RunPod-Key', type=str)
-    key_name = key_name.replace(' ', '-')
-
-    generate_ssh_key_pair(key_name)
+    if not key and not key_file:
+        click.confirm('Would you like to add an SSH key to your account?', abort=True)
+        key_name = click.prompt('Please enter a name for this key', default='RunPod-Key', type=str)
+        key_name = key_name.replace(' ', '-')
+        generate_ssh_key_pair(key_name)
 
     click.echo('The key has been added to your account.')
