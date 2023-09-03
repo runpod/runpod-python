9b99a8d9fe6bbdc7dc281a053f76b084e38e1e7c
Justin Merrell <merrelljustin@gmail.com>
2023-09-03 13:09:47 -0400

    Update test_config.py

--- tests/test_cli/test_config.py
+++ tests/test_cli/test_config.py
@@ -69,7 +69,7 @@ class TestConfig(unittest.TestCase):
 
 
     @patch('os.path.exists', return_value=True)
-    @patch('runpod.cli.config.toml.load')
+    @patch('runpod.cli.config.functions.toml.load')
     @patch('builtins.open', new_callable=mock_open, read_data='[default]\nkey = "value"')
     def test_get_credentials_existing_profile(self, mock_open_call, mock_toml_load, mock_exists):
         '''
@@ -82,7 +82,7 @@ class TestConfig(unittest.TestCase):
         assert mock_exists.called
 
     @patch('os.path.exists', return_value=True)
-    @patch('runpod.cli.config.toml.load')
+    @patch('runpod.cli.config.functions.toml.load')
     @patch('builtins.open', new_callable=mock_open, read_data='[default]\nkey = "value"')
     def test_get_credentials_non_existent_profile(self, mock_open_call, mock_toml_load, mock_exists): # pylint: disable=line-too-long
         '''
