f1b76f14b9dc8632d7958c22bbcb5c5bd24a66b1
Justin Merrell <merrelljustin@gmail.com>
2023-09-04 17:00:40 -0400

    Update pod_info.py

--- runpod/cli/utils/pod_info.py
+++ runpod/cli/utils/pod_info.py
@@ -7,7 +7,7 @@ import time
 def get_ssh_ip_port(pod):
     '''
     Returns the IP and port for SSH access to a pod.
-    Tries up to 3 times with an incremental backoff if necessary.
+    Tries up to 5 times with an incremental backoff if necessary.
     '''
     pod_ip = None
     pod_port = None
@@ -29,10 +29,10 @@ def get_ssh_ip_port(pod):
                 break
 
         # If we have not successfully fetched the IP and port, sleep before retrying
-        if attempt < 2:  # We don't want to sleep after the third attempt
+        if attempt < 5:  # We don't want to sleep after the third attempt
             time.sleep(2 * (attempt + 1))  # Sleeps for 2, 4 seconds
 
     if pod_ip is None or pod_port is None:
-        raise Exception("Failed to retrieve SSH IP and port after 3 attempts.")
+        raise Exception("Failed to retrieve SSH IP and port after 5 attempts.")
 
     return pod_ip, pod_port
