144900f66b8a46c13d4eb9e6f1eeb95c017ddb65
Justin Merrell <merrelljustin@gmail.com>
2023-09-06 17:53:30 -0400

    Update pod_info.py

--- runpod/cli/utils/pod_info.py
+++ runpod/cli/utils/pod_info.py
@@ -2,14 +2,31 @@
 RunPod | CLI | Utils | Pod Info
 '''
 
-def get_ssh_ip_port(pod):
+import time
+
+from runpod import get_pod
+
+def get_ssh_ip_port(pod_id, timeout=60):
     '''
     Returns the IP and port for SSH access to a pod.
     '''
-    if pod['desiredStatus'] == 'RUNNING':
-        for port in pod['runtime']['ports']:
-            if port['privatePort'] == 22:
-                pod_ip = port['ip']
-                pod_port = port['publicPort']
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        pod = get_pod(pod_id)
+        desired_status = pod.get('desiredStatus', None)
+        runtime = pod.get('runtime', None)
 
-    return pod_ip, pod_port
+        if desired_status == 'RUNNING' and runtime:
+            for port in pod['runtime']['ports']:
+                if port['privatePort'] == 22:
+                    return port['ip'], port['publicPort']
+
+        time.sleep(1)
+
+    if desired_status != 'RUNNING':
+        raise TimeoutError(f"Pod {pod_id} did not reach 'RUNNING' state within {timeout} seconds.")
+
+    if runtime:
+        raise TimeoutError(f"Pod {pod_id} did not report runtime data within {timeout} seconds.")
+
+    return None, None
