96e7488d2acdd760cdd7883bce46bbd4ecb54e54
Justin Merrell <merrelljustin@gmail.com>
2023-09-07 15:58:19 -0400

    Update test_project_commands.py

--- tests/test_cli/test_cli_groups/test_project_commands.py
+++ tests/test_cli/test_cli_groups/test_project_commands.py
@@ -5,9 +5,11 @@ import unittest
 from unittest.mock import patch
 
 from click.testing import CliRunner
-from runpod.cli.groups.project.commands import new_project_wizard, launch_project_pod, start_project_pod
+from runpod.cli.groups.project.commands import (
+    new_project_wizard, launch_project_pod, start_project_pod)
 
 class TestProjectCLI(unittest.TestCase):
+    ''' A collection of tests for the Project CLI commands. '''
 
     def setUp(self):
         self.runner = CliRunner()
@@ -16,8 +18,13 @@ class TestProjectCLI(unittest.TestCase):
         '''
         Tests the new_project_wizard command.
         '''
-        with patch('click.prompt', return_value='XYZ_VOLUME') as mock_prompt:
+        with patch('click.prompt', return_value='XYZ_VOLUME') as mock_prompt, \
+             patch('runpod.cli.groups.project.commands.validate_project_name') as mock_validate, \
+             patch('click.confirm', return_value=True) as mock_confirm, \
+             patch('runpod.cli.groups.project.commands.create_new_project') as mock_create:
+            mock_validate.return_value = 'TestProject'
             result = self.runner.invoke(new_project_wizard, ['--name', 'TestProject', '--type', 'llama2', '--model', 'meta-llama/Llama-2-7b']) # pylint: disable=line-too-long
+
         mock_prompt.assert_called_with("Enter the ID of the volume to use", type=str)
         self.assertEqual(result.exit_code, 0)
         self.assertIn("Project TestProject created successfully!", result.output)
@@ -34,10 +41,13 @@ class TestProjectCLI(unittest.TestCase):
         '''
         Tests the launch_project_pod command.
         '''
-        with patch('click.confirm', return_value=True) as mock_confirm:
+        with patch('click.confirm', return_value=True) as mock_confirm, \
+            patch('runpod.cli.groups.project.commands.launch_project') as mock_launch:
             result = self.runner.invoke(launch_project_pod)
+        mock_confirm.assert_called_with("Do you want to continue?", abort=True)
         self.assertEqual(result.exit_code, 0)
         self.assertIn("Launching project development environment...", result.output)
+        mock_launch.assert_called_once()
 
 
     def test_start_project_pod(self):
