dd61f8b6aa65fc5d740c4c9136ed1d6d99418bc8
Justin Merrell <merrelljustin@gmail.com>
2023-09-15 16:32:56 -0400

    linting

--- runpod/cli/utils/rp_sync.py
+++ runpod/cli/utils/rp_sync.py
@@ -1,42 +1,51 @@
+"""
+Watches a directory for changes and syncs them to a remote directory.
+"""
+
 import os
 import time
-# from watchdog.observers import Observer
+import threading
+
 from watchdog.observers.polling import PollingObserver as Observer
 from watchdog.events import FileSystemEventHandler
-import threading
+
 
 IGNORE_EXTENSIONS = ['.tmp', '.log', '.pyc', '.swp']
 IGNORE_FILES = ['ignoreme.txt']
 
 class WatcherHandler(FileSystemEventHandler):
+    """Watches a directory for changes and syncs them to a remote directory."""
+
     def __init__(self, ssh_client, local_path, remote_path):
         self.ssh_client = ssh_client
         self.local_path = local_path
         self.remote_path = remote_path
 
     def should_ignore(self, file_path):
-        # Check for ignored extensions
+        """
+        Returns True if the file should be ignored, False otherwise.
+        """
         if any(file_path.endswith(ext) for ext in IGNORE_EXTENSIONS):
             return True
 
-        # Check for ignored filenames
         if os.path.basename(file_path) in IGNORE_FILES:
             return True
 
         return False
 
-    # def dispatch(self, event):
-    #     print(f"Event {event.event_type} triggered for {event.src_path}.")
-    #     super().dispatch(event)
-
-
     def on_modified(self, event):
+        """
+        Called when a file is modified.
+        """
         if event.is_directory or self.should_ignore(event.src_path):
             return
         print(f'File {event.src_path} has been modified.')
         self.ssh_client.rsync(os.path.join(self.local_path, ''), self.remote_path)
 
     def on_moved(self, event):
+        """
+        Called when a file is moved or renamed.
+        """
         if event.is_directory or self.should_ignore(event.dest_path):
             return
         print(f'File {event.dest_path} has been moved or renamed.')
@@ -44,16 +53,25 @@ class WatcherHandler(FileSystemEventHandler):
 
 
     def on_created(self, event):
+        """
+        Called when a file is created.
+        """
         if event.is_directory or self.should_ignore(event.src_path):
             return
         print(f'File {event.src_path} has been created.')
 
     def on_deleted(self, event):
+        """
+        Called when a file is deleted.
+        """
         if event.is_directory or self.should_ignore(event.src_path):
             return
         print(f'File {event.src_path} has been deleted.')
 
 def start_watcher(ssh_client, local_path, remote_path):
+    """
+    Starts the watcher.
+    """
     event_handler = WatcherHandler(ssh_client, local_path, remote_path)
     observer = Observer()
     observer.schedule(event_handler, local_path, recursive=True)
@@ -68,5 +86,7 @@ def start_watcher(ssh_client, local_path, remote_path):
     observer.join()
 
 def sync_directory(ssh_client, local_path, remote_path):
-    t = threading.Thread(target=start_watcher, args=(ssh_client, local_path, remote_path,))
-    t.start()
+    """
+    Syncs a local directory to a remote directory.
+    """
+    threading.Thread(target=start_watcher, args=(ssh_client, local_path, remote_path)).start()
