e4bac11e40451ba4847487031fc65c4f9c8d6923
Justin Merrell <merrelljustin@gmail.com>
2023-09-20 18:56:04 -0400

    Create test_progress.py

---
+++ tests/test_serverless/test_modules/test_progress.py
@@ -0,0 +1,37 @@
+"""
+Tests for the rp_progress.py module.
+"""
+
+
+import unittest
+from unittest.mock import patch, Mock
+from runpod.serverless import progress_update
+
+class TestProgressUpdate(unittest.TestCase):
+
+    @patch("runpod.serverless.modules.rp_progress.os.environ.get")
+    @patch("runpod.serverless.modules.rp_progress.aiohttp.ClientSession")
+    @patch("runpod.serverless.modules.rp_progress.send_result")
+    def test_progress_update(self, mock_send_result, mock_client_session, mock_os_get):
+        """
+        Tests that the progress_update function calls the send_result function with the correct
+        """
+        # Set mock values
+        mock_os_get.return_value = "fake_api_key"
+        fake_session = Mock()
+        mock_client_session.return_value = fake_session
+
+        # Call the function
+        job = "fake_job"
+        progress = "50%"
+        progress_update(job, progress)
+
+        # Assertions
+        mock_os_get.assert_called_once_with('RUNPOD_AI_API_KEY')
+        mock_client_session.assert_called_once()
+
+        expected_job_data = {
+            "status": "IN_PROGRESS",
+            "output": progress
+        }
+        mock_send_result.assert_called_once_with(fake_session, expected_job_data, job)
