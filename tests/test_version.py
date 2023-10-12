58fe3e0dba81f7709c94cde794f72d154ec54199
Justin Merrell <merrelljustin@gmail.com>
2023-10-12 19:44:45 -0400

    Update test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,8 +1,10 @@
 """ Test the version module """
+import importlib
+
 from unittest.mock import MagicMock, patch
 from pkg_resources import DistributionNotFound
 
-import runpod
+import runpod.version
 
 def test_version_found():
     """Test that the version is found"""
@@ -11,6 +13,8 @@ def test_version_found():
         mock_distribution.version = '1.0.0'
         mock_get_distribution.return_value = mock_distribution
 
+        importlib.reload(runpod.version)
+
         assert mock_get_distribution.called
         assert runpod.__version__ == '1.0.0'
 
@@ -18,6 +22,8 @@ def test_version_not_found():
     """Test that the version is unknown"""
     with patch('runpod.version.get_distribution') as mock_get_distribution:
         mock_get_distribution.side_effect = DistributionNotFound
+
+        importlib.reload(runpod.version)
 
         assert mock_get_distribution.called
         assert runpod.__version__ == 'unknown'
