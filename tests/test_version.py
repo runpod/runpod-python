168d76003b8fbd4aa8d393390af747c11e525eae
Justin Merrell <merrelljustin@gmail.com>
2023-10-12 19:57:28 -0400

    fix: refactor version fetching

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,29 +1,17 @@
 """ Test the version module """
-import importlib
 
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch, Mock
 from pkg_resources import DistributionNotFound
-
-import runpod.version
+from runpod.version import get_version
 
 def test_version_found():
-    """Test that the version is found"""
-    with patch('runpod.version.get_distribution') as mock_get_distribution:
-        mock_distribution = MagicMock()
-        mock_distribution.version = '1.0.0'
-        mock_get_distribution.return_value = mock_distribution
-
-        importlib.reload(runpod.version)
-
+    """ Test that the version is found """
+    with patch('runpod.version.get_distribution', return_value=Mock(version='1.0.0')) as mock_get_distribution:
+        assert get_version() == '1.0.0'
         assert mock_get_distribution.called
-        assert runpod.__version__ == '1.0.0'
 
 def test_version_not_found():
-    """Test that the version is unknown"""
-    with patch('runpod.version.get_distribution') as mock_get_distribution:
-        mock_get_distribution.side_effect = DistributionNotFound
-
-        importlib.reload(runpod.version)
-
+    """ Test that the version is not found """
+    with patch('runpod.version.get_distribution', side_effect=DistributionNotFound) as mock_get_distribution:
+        assert get_version() == 'unknown'
         assert mock_get_distribution.called
-        assert runpod.__version__ == 'unknown'
