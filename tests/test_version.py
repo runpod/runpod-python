b4b7cd0d2921a8736a1312cda7e49436f782ffdf
Justin Merrell <merrelljustin@gmail.com>
2023-10-12 20:04:55 -0400

    fix: exclude build directory in tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,12 +6,14 @@ from runpod.version import get_version
 
 def test_version_found():
     """ Test that the version is found """
-    with patch('runpod.version.get_distribution', return_value=Mock(version='1.0.0')) as mock_get_distribution:
+    with patch('runpod.version.get_distribution') as mock_get_distribution:
+        mock_get_distribution.return_value = Mock(version='1.0.0')
         assert get_version() == '1.0.0'
         assert mock_get_distribution.called
 
 def test_version_not_found():
     """ Test that the version is not found """
-    with patch('runpod.version.get_distribution', side_effect=DistributionNotFound) as mock_get_distribution:
+    with patch('runpod.version.get_distribution') as mock_get_distribution:
+        mock_get_distribution.side_effect = DistributionNotFound
         assert get_version() == 'unknown'
         assert mock_get_distribution.called
