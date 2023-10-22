cb35e8cf6521afdc92d6819cda86caec41359fb8
Justin Merrell <merrelljustin@gmail.com>
2023-10-22 12:22:04 -0400

    Update test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,19 +1,19 @@
 """ Test the version module """
 
 from unittest.mock import patch, Mock
-from pkg_resources import DistributionNotFound
+from importlib.metadata import PackageNotFoundError
 from runpod.version import get_version
 
 def test_version_found():
     """ Test that the version is found """
-    with patch('runpod.version.get_distribution') as mock_get_distribution:
-        mock_get_distribution.return_value = Mock(version='1.0.0')
+    with patch('runpod.version.version') as mock_get_version:
+        mock_get_version.return_value = Mock(version='1.0.0')
         assert get_version() == '1.0.0'
-        assert mock_get_distribution.called
+        assert mock_get_version.called
 
 def test_version_not_found():
     """ Test that the version is not found """
-    with patch('runpod.version.get_distribution') as mock_get_distribution:
-        mock_get_distribution.side_effect = DistributionNotFound
+    with patch('runpod.version.version') as mock_get_version:
+        mock_get_version.side_effect = PackageNotFoundError
         assert get_version() == 'unknown'
-        assert mock_get_distribution.called
+        assert mock_get_version.called
