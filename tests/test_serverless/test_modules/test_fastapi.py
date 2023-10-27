522492225b4b72d69be5e56c5a0f1407d64a3418
Justin Merrell <merrelljustin@gmail.com>
2023-10-27 18:51:13 -0400

    Update test_fastapi.py

--- tests/test_serverless/test_modules/test_fastapi.py
+++ tests/test_serverless/test_modules/test_fastapi.py
@@ -91,8 +91,8 @@ class TestFastAPI(unittest.TestCase):
             def generator_handler():
                 yield {"result": "success"}
 
-            generator_run_return = asyncio.run(worker_api._run(job_object)) # pylint: disable=protected-access
-
+            generator_worker_api = rp_fastapi.WorkerAPI(handler=generator_handler)
+            generator_run_return = asyncio.run(generator_worker_api._run(job_object)) # pylint: disable=protected-access
             assert generator_run_return == {"output": {"result": "success"}}
 
         loop.close()
