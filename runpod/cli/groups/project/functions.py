cd686a268770e33346ccd75bfb9ee8b723a0d72f
Justin Merrell <merrelljustin@gmail.com>
2023-10-29 19:08:42 -0400

    Update functions.py

--- runpod/cli/groups/project/functions.py
+++ runpod/cli/groups/project/functions.py
@@ -270,8 +270,6 @@ def start_project_api():
                 python -m pip install --upgrade pip && python -m pip install -r {requirements_path}
             fi
 
-            sleep 1 #Debounce
-
             python {handler_path} --rp_serve_api --rp_api_host="0.0.0.0" --rp_api_port=8080 --rp_api_concurrency=1 &
             last_pid=$!
 
