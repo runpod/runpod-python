1bdda613db5d703aebbae56d282f2577857397da
Justin Merrell <merrelljustin@gmail.com>
2023-11-03 08:58:45 -0400

    fix: unit test

--- tests/test_cli/test_cli_groups/test_project_functions.py
+++ tests/test_cli/test_cli_groups/test_project_functions.py
@@ -139,6 +139,7 @@ class TestStartProject(unittest.TestCase):
         }
 
         mock_ssh_instance = mock_ssh_connection.return_value
+        mock_ssh_instance.__enter__.return_value = mock_ssh_instance
         mock_ssh_instance.run_commands.return_value = None
 
         with patch('runpod.cli.groups.project.functions.sync_directory') as mock_sync_directory:
