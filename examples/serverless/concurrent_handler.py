fd73d0c37ea98effd99afb75fb0cbe83503939cf
Justin Merrell <merrelljustin@gmail.com>
2023-11-19 16:11:57 -0500

    Update concurrent_handler.py

--- examples/serverless/concurrent_handler.py
+++ examples/serverless/concurrent_handler.py
@@ -8,20 +8,44 @@ async def async_generator_handler(job):
     return
 
 
-def concurrency_controller():
+max_number_of_jobs_allowed = 10
+
+
+vllm_in_queue = 3  # Function
+
+
+def concurrency_controller(desired_concurrency=1):  # get desired concurrency
     '''
     Concurrency controller.
     '''
-    return True
+    # max_number_of_jobs_allowed = 1
+
+    # if num_of_running_jobs < 1:
+    #     max_number_of_jobs_allowed = max_number_of_jobs_allowed
+
+    # if num_of_running_jobs >= 1:
+    #     max_number_of_jobs_allowed = max_number_of_jobs_allowed
+
+    if vllm_in_queue < 10:
+        max_number_of_jobs_allowed = max_number_of_jobs_allowed + 1
+
+    if vllm_in_queue >= 10:
+        max_number_of_jobs_allowed = max_number_of_jobs_allowed - 1
+
+    return max_number_of_jobs_allowed
+
+
+def concurrency_modifier(current_concurrency=1):
+    '''
+    Concurrency modifier.
+    '''
+
+    desired_concurrency = current_concurrency
+
+    return desired_concurrency
 
 
 runpod.serverless.start({
     "handler": async_generator_handler,
-    "concurrency_controller": concurrency_controller,
-    "concurrency_config": {
-        "min_concurrent_requests": 2,
-        "max_concurrent_requests": 100,
-        "concurrency_scale_factor": 4,
-        "availability_ratio_threshold": 0.90
-    }
+    "concurrency_controller": concurrency_controller
 })
