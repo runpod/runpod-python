89e62720692ba1c673d2c02654ecdc3e8749413c
Justin Merrell <merrelljustin@gmail.com>
2023-12-13 11:25:24 -0500

    Update test_fastapi.py

--- tests/test_serverless/test_modules/test_fastapi.py
+++ tests/test_serverless/test_modules/test_fastapi.py
@@ -24,7 +24,8 @@ class TestFastAPI(unittest.TestCase):
         Tests the start_serverless() method with the realtime option.
         '''
         module_location = "runpod.serverless.modules.rp_fastapi"
-        with patch(f"{module_location}.FastAPI", Mock()) as mock_fastapi, \
+        with patch(f"{module_location}.Heartbeat.start_ping", Mock()) as mock_ping, \
+                patch(f"{module_location}.FastAPI", Mock()) as mock_fastapi, \
                 patch(f"{module_location}.APIRouter", return_value=Mock()) as mock_router, \
                 patch(f"{module_location}.uvicorn", Mock()) as mock_uvicorn:
 
@@ -105,8 +106,7 @@ class TestFastAPI(unittest.TestCase):
         loop = asyncio.get_event_loop()
 
         module_location = "runpod.serverless.modules.rp_fastapi"
-        with patch(f"{module_location}.Heartbeat.start_ping", Mock()) as mock_ping, \
-                patch(f"{module_location}.FastAPI", Mock()), \
+        with patch(f"{module_location}.FastAPI", Mock()), \
                 patch(f"{module_location}.APIRouter", return_value=Mock()), \
                 patch(f"{module_location}.uvicorn", Mock()), \
                 patch(f"{module_location}.uuid.uuid4", return_value="123"):
