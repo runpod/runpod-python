9188e0a2770f1e4b78d304dd3e6c24b23fd8c130
Justin Merrell <merrelljustin@gmail.com>
2024-02-10 09:03:03 -0500

    Update test_worker.py

--- tests/test_serverless/test_worker.py
+++ tests/test_serverless/test_worker.py
@@ -57,14 +57,14 @@ class TestWorker(IsolatedAsyncioTestCase):
             assert runpod.serverless.worker._is_local(
                 self.mock_config) is False
 
-    async def test_start(self):
+    def test_start(self):
         '''
         Test basic start call.
         '''
         with patch("builtins.open", mock_open(read_data='{"input":{"number":1}}')) as mock_file, \
                 self.assertRaises(SystemExit):
 
-            await runpod.serverless.start({"handler": self.mock_handler})
+            runpod.serverless.start({"handler": self.mock_handler})
 
             assert mock_file.called
 
