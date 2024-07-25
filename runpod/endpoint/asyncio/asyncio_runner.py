d7e82cc7d0ede39d76f5f5972da98aac4e0ead67
Dean Quiñanola <dean.quinanola@runpod.io>
2024-07-24 17:35:17 -0700

    asyncio_runner uses runpod.http_client.AsyncClientSession

--- runpod/endpoint/asyncio/asyncio_runner.py
+++ runpod/endpoint/asyncio/asyncio_runner.py
@@ -3,15 +3,15 @@
 
 from typing import Any, Dict
 import asyncio
-import aiohttp
 
+from runpod.http_client import AsyncClientSession
 from runpod.endpoint.helpers import FINAL_STATES, is_completed
 
 
 class Job:
     """Class representing a job for an asynchronous endpoint"""
 
-    def __init__(self, endpoint_id: str, job_id: str, session: aiohttp.ClientSession):
+    def __init__(self, endpoint_id: str, job_id: str, session: AsyncClientSession):
         from runpod import api_key, endpoint_url_base  # pylint: disable=import-outside-toplevel,cyclic-import
 
         self.endpoint_id = endpoint_id
@@ -100,7 +100,7 @@ class Job:
 class Endpoint:
     """Class for running endpoint"""
 
-    def __init__(self, endpoint_id: str, session: aiohttp.ClientSession):
+    def __init__(self, endpoint_id: str, session: AsyncClientSession):
         from runpod import api_key, endpoint_url_base  # pylint: disable=import-outside-toplevel
 
         self.endpoint_id = endpoint_id
