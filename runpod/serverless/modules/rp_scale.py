f55002377492f095129db1f871006f4380a94c7c
Dean Quiñanola <dean.quinanola@runpod.io>
2024-09-03 22:22:40 -0700

    restored rp_scale to 1.6.2

--- runpod/serverless/modules/rp_scale.py
+++ runpod/serverless/modules/rp_scale.py
@@ -5,6 +5,7 @@ Provides the functionality for scaling the runpod serverless worker.
 
 import asyncio
 import typing
+
 from runpod.serverless.modules.rp_logger import RunPodLogger
 from .rp_job import get_job
 from .worker_state import Jobs
@@ -16,9 +17,12 @@ job_list = Jobs()
 def _default_concurrency_modifier(current_concurrency: int) -> int:
     """
     Default concurrency modifier.
+
     This function returns the current concurrency without any modification.
+
     Args:
         current_concurrency (int): The current concurrency.
+
     Returns:
         int: The current concurrency.
     """
@@ -36,6 +40,8 @@ class JobScaler():
         else:
             self.concurrency_modifier = concurrency_modifier
 
+        self.background_get_job_tasks = set()
+        self.job_history = []
         self.current_concurrency = 1
         self._is_alive = True
 
@@ -55,27 +61,25 @@ class JobScaler():
         """
         Retrieve multiple jobs from the server in parallel using concurrent requests.
 
-        Yields:
-            Dict[str, Any]: A job data retrieved from the server.
+        Returns:
+            List[Any]: A list of job data retrieved from the server.
         """
         while self.is_alive():
             self.current_concurrency = self.concurrency_modifier(self.current_concurrency)
             log.debug(f"Concurrency set to: {self.current_concurrency}")
 
-            job_count = job_list.get_job_count()
-            log.debug(f"Jobs in progress: {job_count}")
-
-            if job_count < self.current_concurrency:
+            log.debug(f"Jobs in progress: {job_list.get_job_count()}")
+            if job_list.get_job_count() < self.current_concurrency and self.is_alive():
                 log.debug("Job list is less than concurrency, getting more jobs.")
-                # TODO: use the new batch job take
+
                 tasks = [
                     asyncio.create_task(get_job(session, retry=False))
-                    for _ in range(self.current_concurrency - job_count)
+                    for _ in range(self.current_concurrency if job_list.get_job_list() else 1)
                 ]
 
                 for job_future in asyncio.as_completed(tasks):
-                    # TODO: yield the batched jobs returned
                     job = await job_future
+                    self.job_history.append(1 if job else 0)
                     if job:
                         yield job
 
