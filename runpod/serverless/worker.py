43372e249f4dd8c5dae5b9f4553eb89a8330ce60
Dean Quiñanola <dean.quinanola@runpod.io>
2024-09-25 14:30:48 -0700

    Moved _process_job to JobScaler.process_job

--- runpod/serverless/worker.py
+++ runpod/serverless/worker.py
@@ -8,15 +8,9 @@ import os
 from typing import Any, Dict
 
 from runpod.http_client import AsyncClientSession
-from runpod.serverless.modules import rp_handler, rp_local, rp_logger, rp_ping, rp_scale
-
-from .modules.rp_http import send_result, stream_result
-from .modules.rp_job import run_job, run_job_generator
-from .modules.worker_state import REF_COUNT_ZERO, Jobs
-from .utils import rp_debugger
+from runpod.serverless.modules import rp_logger, rp_local, rp_ping, rp_scale
 
 log = rp_logger.RunPodLogger()
-job_list = Jobs()
 heartbeat = rp_ping.Heartbeat()
 
 
@@ -29,48 +23,6 @@ def _is_local(config) -> bool:
         return True
 
     return False
-
-
-async def _process_job(job, session, job_scaler, config):
-    if rp_handler.is_generator(config["handler"]):
-        is_stream = True
-        generator_output = run_job_generator(config["handler"], job)
-        log.debug("Handler is a generator, streaming results.", job["id"])
-
-        job_result = {"output": []}
-        async for stream_output in generator_output:
-            log.debug(f"Stream output: {stream_output}", job["id"])
-            if "error" in stream_output:
-                job_result = stream_output
-                break
-            if config.get("return_aggregate_stream", False):
-                job_result["output"].append(stream_output["output"])
-
-            await stream_result(session, stream_output, job)
-    else:
-        is_stream = False
-        job_result = await run_job(config["handler"], job)
-
-    # If refresh_worker is set, pod will be reset after job is complete.
-    if config.get("refresh_worker", False):
-        log.info("refresh_worker flag set, stopping pod after job.", job["id"])
-        job_result["stopPod"] = True
-        job_scaler.kill_worker()
-
-    # If rp_debugger is set, debugger output will be returned.
-    if config["rp_args"].get("rp_debugger", False) and isinstance(job_result, dict):
-        job_result["output"]["rp_debugger"] = rp_debugger.get_debugger_output()
-        log.debug("rp_debugger | Flag set, returning debugger output.", job["id"])
-
-        # Calculate ready delay for the debugger output.
-        ready_delay = (config["reference_counter_start"] - REF_COUNT_ZERO) * 1000
-        job_result["output"]["rp_debugger"]["ready_delay_ms"] = ready_delay
-    else:
-        log.debug("rp_debugger | Flag not set, skipping debugger output.", job["id"])
-        rp_debugger.clear_debugger_output()
-
-    # Send the job result to SLS
-    await send_result(session, job_result, job, is_stream=is_stream)
 
 
 # ------------------------- Main Worker Running Loop ------------------------- #
