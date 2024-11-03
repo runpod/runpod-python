782bdd8ea46aea99422071fd290de9f484951b16
gabewillen <gabewillen@gmail.com>
2024-11-03 01:18:08 -0500

    add tests for is_generator modifications

--- tests/test_serverless/test_modules/test_handler.py
+++ tests/test_serverless/test_modules/test_handler.py
@@ -9,6 +9,46 @@ from runpod.serverless.modules.rp_handler import is_generator
 class TestIsGenerator(unittest.TestCase):
     """Tests for the is_generator function."""
 
+    def test_callable_non_generator_object(self):
+        """Test that a callable object is not a generator."""
+
+        class CallableObject:
+            def __call__(self):
+                return "I'm a callable object!"
+
+        callable_obj = CallableObject()
+        self.assertFalse(is_generator(callable_obj))
+
+    def test_callable_object_generator_object(self):
+        """Test that a callable object with a generator method is a generator."""
+
+        class CallableGeneratorObject:
+            def __call__(self):
+                yield "I'm a callable object with a generator method!"
+
+        callable_obj = CallableGeneratorObject()
+        self.assertTrue(is_generator(callable_obj))
+
+    def test_async_callable_non_generator_object(self):
+        """Test that an async callable object is not a generator."""
+
+        class AsyncCallableNonGeneratorObject:
+            async def __call__(self):
+                return "I'm an async callable object!"
+
+        async_callable_obj = AsyncCallableNonGeneratorObject()
+        self.assertFalse(is_generator(async_callable_obj))
+
+    def test_async_callable_generator_object(self):
+        """Test that an async callable object with a generator method is a generator."""
+
+        class AsyncCallableGeneratorObject:
+            async def __call__(self):
+                yield "I'm an async callable object with a generator method!"
+
+        async_callable_obj = AsyncCallableGeneratorObject()
+        self.assertTrue(is_generator(async_callable_obj))
+
     def test_regular_function(self):
         """Test that a regular function is not a generator."""
 
